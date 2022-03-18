a9e0ef531e45c8ca3fd345f41bb74975b6a0784b
harrysolovay <harrysolovay@gmail.com>
2022-03-17 22:44:28 -0400

    add chain_getFinalizedHead effect

---
+++ rpc/ChainGetFinalizedHead.ts
@@ -0,0 +1,12 @@
+import * as u from "/_/util/mod.ts";
+import * as common from "/rpc/common.ts";
+import * as sys from "/system/mod.ts";
+
+export const ChainGetFinalizedHead = <
+  Beacon,
+  Resource extends sys.AnyEffectA<sys.ResourceResolved<Beacon>>,
+>(resource: Resource) => {
+  return sys.effect<string>()("ChainGetBlockHash", { resource }, async (_, resolved) => {
+    return common.call(resolved.resource, "chain_getFinalizedHead", u.isString);
+  });
+};
--- rpc/mod.ts
+++ rpc/mod.ts
@@ -1,5 +1,6 @@
 export * from "./ChainGetBlock.ts";
 export * from "./ChainGetBlockHash.ts";
+export * from "./ChainGetFinalizedHead.ts";
 export * from "./RpcMethods.ts";
 export * from "./StateGetMetadata.ts";
 export * from "./StateGetStorage.ts";
