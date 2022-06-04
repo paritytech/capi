fa51e95e2add943930f8c500eb7b3ddd18b113af
harrysolovay <harrysolovay@gmail.com>
2022-06-04 08:44:46 +0200

    only use paged rpc call

--- effect/std/mapKeys.ts
+++ effect/std/mapKeys.ts
@@ -22,10 +22,8 @@ export const mapKeys = effector.async(
       const palletMetadata_ = palletMetadata(metadataLookup_, map.pallet.name);
       const entryMetadata_ = entryMetadata(metadataLookup_, palletMetadata_, map.name);
       const key = entryKey(deriveCodec_, palletMetadata_, entryMetadata_);
-      // TODO: clean up typings if one is missing
-      const rpcCall_ = count !== undefined
-        ? rpcCall(map.pallet.rpc, "state_getKeysPaged", key, count, startKey)
-        : rpcCall(map.pallet.rpc, "state_getKeys", key);
+      // TODO: why is `undefined` an invalid argument for `count`?
+      const rpcCall_ = rpcCall(map.pallet.rpc, "state_getKeysPaged", key, count!, startKey);
       const keys = select(rpcCall_, "result");
       return wrap(keys, "keys").run();
     },
