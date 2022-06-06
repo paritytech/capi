c4737f05b4ac58cbfdad5f6d525bff6d768590a0
harrysolovay <harrysolovay@gmail.com>
2022-06-06 23:07:50 +0100

    apply 140 formatter

--- effect/std/atoms/select.ts
+++ effect/std/atoms/select.ts
@@ -3,5 +3,6 @@ import { effector, EffectorArgs } from "/effect/Effect.ts";
 export const select = effector.sync.generic(
   "select",
   (effect) =>
-    <T, K extends keyof T, X extends unknown[]>(...args: EffectorArgs<X, [target: T, key: K]>) => effect(args, () => (target, key): T[K] => target[key]),
+    <T, K extends keyof T, X extends unknown[]>(...args: EffectorArgs<X, [target: T, key: K]>) =>
+      effect(args, () => (target, key): T[K] => target[key]),
 );
