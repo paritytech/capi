5da706db6204270cbabd7c11152fdcd16545aac4
Harry Solovay <harrysolovay@gmail.com>
2022-06-14 14:23:55 +0200

    continued

--- spec/Extrinsic.ts
+++ spec/Extrinsic.ts
@@ -3,15 +3,19 @@ import { ExtrinsicFactory } from "./ExtrinsicFactory.ts";
 import { NodeBase, NodeKind } from "./Node.ts";
 import { SignedExtrinsic } from "./SignedExtrinsic.ts";
 
-export class Extrinsic<CallData extends Record<string, unknown> = Record<string, any>>
-  extends NodeBase
-{
+export class Extrinsic<
+  CallData extends Record<string, unknown> = Record<string, any>,
+  Extra extends unknown[] = any[],
+  Additional extends unknown = any,
+> extends NodeBase {
   readonly kind = NodeKind.Extrinsic;
 
   constructor(
     readonly factory: ExtrinsicFactory<CallData>,
     readonly caller: MultiAddress,
     readonly callData: CallData,
+    readonly extra?: Extra,
+    readonly additional?: Additional,
   ) {
     super();
   }
--- spec/ExtrinsicFactory.ts
+++ spec/ExtrinsicFactory.ts
@@ -4,20 +4,28 @@ import { Extrinsic } from "./Extrinsic.ts";
 import { NodeBase, NodeKind } from "./Node.ts";
 import { Pallet } from "./Pallet.ts";
 
-export class ExtrinsicFactory<CallData extends Record<string, unknown> = Record<string, any>>
-  extends NodeBase
-{
+export class ExtrinsicFactory<
+  CallData extends Record<string, unknown> = Record<string, any>,
+  Extra extends unknown[] = any[],
+  Additional extends unknown = any,
+> extends NodeBase {
   readonly kind = NodeKind.ExtrinsicFactory;
 
   constructor(
     readonly pallet: Pallet,
     readonly methodName: string,
     readonly callDataCodec?: Codec<CallData>,
+    readonly extraCodec?: Codec<Extra>,
   ) {
     super();
   }
 
-  create(caller: MultiAddress, callData: CallData): Extrinsic<CallData> {
-    return new Extrinsic(this, caller, callData);
+  create(
+    caller: MultiAddress,
+    callData: CallData,
+    extra?: Extra,
+    additional?: Additional,
+  ): Extrinsic<CallData> {
+    return new Extrinsic(this as any, caller, callData, extra, additional);
   }
 }
