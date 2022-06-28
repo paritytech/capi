b3f795cee3338dda64c6cc2b50802a6287c98eec
wirednkod <wirednkod@gmail.com>
2022-06-28 13:27:00 +0300

    Fix typo; correct lint

--- rpc/providers/proxy.test.ts
+++ rpc/providers/proxy.test.ts
@@ -1,19 +1,19 @@
-import { KnownRpcMethods } from "../../known/mod.ts";
-import { proxyClient, ProxyBeacon } from "./proxy.ts"
-import { node } from "../../test-util/node.ts";
 import { assert, assertEquals } from "../../_deps/asserts.ts";
+import { KnownRpcMethods } from "../../known/mod.ts";
+import { node } from "../../test-util/node.ts";
+import { proxyClient, ProxyBeacon } from "./proxy.ts";
 
 Deno.test({
-  name: "Test proxClient flow",
+  name: "Test proxyClient flow",
   fn: async (s) => {
     // Initialize local node
-    const process = await node()
+    const process = await node();
     // Create the proxyClient
     const client = await proxyClient(new ProxyBeacon<KnownRpcMethods>(process.url), {
       close() {},
       send(_msg) {},
       receive(_msg) {},
-      error() {}
+      error() {},
     });
     // Make sure that client did not return an error
     assert(!(client instanceof Error));
@@ -22,33 +22,33 @@ Deno.test({
     await s.step({
       name: "Tests correct functionality",
       fn: async () => {
-        const raw = await client.call("state_getMetadata", [])
-        assert(raw.result)
+        const raw = await client.call("state_getMetadata", []);
+        assert(raw.result);
       }
-    })
+    });
 
     // even though CAPI is not allowing this so this tests should probably not be here
     await s.step({
       name: "Tests error when a call is made with wrong method",
       fn: async () => {
-        const raw = await client.call("state_getMetaSomething" as keyof KnownRpcMethods, [])
-        assert(raw.error)
-        assertEquals(raw.error.message, "Method not found")
-      }
-    })
+        const raw = await client.call("state_getMetaSomething" as keyof KnownRpcMethods, []);
+        assert(raw.error);
+        assertEquals(raw.error.message, "Method not found");
+      },
+    });
 
     await s.step({
       name: "Tests that id increments correctly",
       fn: async () => {
-        const raw = await client.call("state_getMetadata", [])
-        assertEquals(raw.id, "2")
-      }
-    })
+        const raw = await client.call("state_getMetadata", []);
+        assertEquals(raw.id, "2");
+      },
+    });
 
     // Finally
-    await client.close()
-    process.close()
+    await client.close();
+    process.close();
   },
-  sanitizeResources: false
+  sanitizeResources: false,
 });
 
