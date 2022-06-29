8198dc6d610837afb0c855f79ec113f728f42f19
wirednkod <wirednkod@gmail.com>
2022-06-29 11:04:52 +0300

    Fix tests for send and subscribe - add spies

--- rpc/providers/proxy.test.ts
+++ rpc/providers/proxy.test.ts
@@ -1,29 +1,49 @@
-import { assert, assertEquals } from "../../_deps/asserts.ts";
+import { assert, assertEquals, } from "../../_deps/asserts.ts";
+import {
+  assertSpyCall,
+  assertSpyCalls,
+  spy,
+} from "../../_deps/mock.ts";
 import { KnownRpcMethods } from "../../known/mod.ts";
 import { node } from "../../test-util/node.ts";
-import { proxyClient, ProxyBeacon } from "./proxy.ts";
+import { ProxyBeacon, proxyClient } from "./proxy.ts";
+
+const delay = (ms: number) => {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 Deno.test({
-  name: "Test proxyClient flow",
-  fn: async (s) => {
+  name: "Test proxyClient send",
+  fn: async (s: Deno.TestContext) => {
     // Initialize local node
     const process = await node();
+
     // Create the proxyClient
-    const client = await proxyClient(new ProxyBeacon<KnownRpcMethods>(process.url), {
-      close() {},
-      send(_msg) {},
-      receive(_msg) {},
-      error() {},
-    });
+    const hooks = {
+      close: spy(() => {}),
+      send: spy((_msg) => {}),
+      receive: spy((_msg) => {}),
+      error: spy((_err) => {}),
+    }
+    const client = await proxyClient(new ProxyBeacon<KnownRpcMethods>(process.url), hooks);
     // Make sure that client did not return an error
     assert(!(client instanceof Error));
 
     // start running tests
     await s.step({
-      name: "Tests correct functionality",
+      name: "Tests send",
       fn: async () => {
+        const jsonSend = {
+          id: "0",
+          jsonrpc: "2.0",
+          method: "state_getMetadata",
+          params: []
+        }
         const raw = await client.call("state_getMetadata", []);
+        assertSpyCalls(hooks.send, 1)
         assert(raw.result);
+        assertSpyCall(hooks.send, 0, { args: [jsonSend] })
+        assertSpyCalls(hooks.receive, 1)
       }
     });
 
@@ -31,9 +51,13 @@ Deno.test({
     await s.step({
       name: "Tests error when a call is made with wrong method",
       fn: async () => {
-        const raw = await client.call("state_getMetaSomething" as keyof KnownRpcMethods, []);
-        assert(raw.error);
-        assertEquals(raw.error.message, "Method not found");
+        const expectedError = {
+          id: "1",
+          jsonrpc: "2.0",
+          error: { code: -32601, message: "Method not found" }
+        }
+        await client.call("state_getMetaSomething" as keyof KnownRpcMethods, []);
+        assertSpyCall(hooks.receive, 1, { args: [expectedError]})
       },
     });
 
@@ -45,10 +69,26 @@ Deno.test({
       },
     });
 
-    // Finally
+    await s.step({
+      name: "Tests proxyClient subscribe",
+      fn: async () => {
+        const listenerSpy = spy((): void => {});
+
+        await client.subscribe(
+          "chain_subscribeNewHead",
+          [],
+          listenerSpy
+        );
+        // 5 seconds delay to ensure that 2 heads were received
+        await delay(5000);
+        assertSpyCalls(hooks.receive, 6);
+        assertSpyCalls(listenerSpy, 2);
+      },
+    });
+
     await client.close();
+    assertSpyCalls(hooks.close, 1);
     process.close();
   },
   sanitizeResources: false,
 });
-
