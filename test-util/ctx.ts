b5324e43242e2ca5020c6c379209488f33c5fc51
Harry Solovay <harrysolovay@gmail.com>
2022-07-21 16:57:06 +0800

    fix old name usage in ctx

--- test-util/ctx.ts
+++ test-util/ctx.ts
@@ -1,10 +1,10 @@
 import { config } from "./config.ts";
-import { node, TestNodeConfig } from "./node.ts";
+import { node, NodeConfig } from "./node.ts";
 
 // TODO: generic over config
 export async function ctx(
   use: (config_: ReturnType<typeof config>) => Promise<void>,
-  nodeConfig?: TestNodeConfig,
+  nodeConfig?: NodeConfig,
 ) {
   const node_ = await node(nodeConfig);
   await use(config(node_));
