c15aaea03c448c66d0d3b7e947a5bde0ab121f2f
Harry Solovay <harrysolovay@gmail.com>
2022-07-23 13:01:21 +0800

    get rid of code meant for other pr

--- effect/mod.ts
+++
@@ -1,7 +0,0 @@
-export * from "./atoms/mod.ts";
-export * from "./run.ts";
-export * from "./std/block/read.ts";
-export * from "./std/entry/read.ts";
-export * from "./std/entry/watch.ts";
-export * from "./std/keyPage/read.ts";
-export * from "./sys/mod.ts";
