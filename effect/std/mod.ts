7baaecf59453c478e132e45ebc7be2e9304a64a6
Harry Solovay <harrysolovay@gmail.com>
2022-07-29 20:13:51 +0800

    cleanup

--- effect/std/mod.ts
+++ effect/std/mod.ts
@@ -1,5 +1,4 @@
 export * from "./readBlock.ts";
-export * from "./readContractEntry.ts";
 export * from "./readEntry.ts";
 export * from "./readKeyPage.ts";
 export * from "./submitAndWatchExtrinsic.ts";
