fef82e2f819bccd9c86297e74e920b53976fdefe
Harry Solovay <harrysolovay@gmail.com>
2022-08-03 14:23:32 +0800

    add effect exports

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 export * from "./config/mod.ts";
 export * as $ from "./deps/scale.ts";
 export * from "./effect/mod.ts";
+export * as E from "./effect/mod.ts";
 export * from "./fluent/mod.ts";
 export * as M from "./frame_metadata/mod.ts";
 export { kusama, moonbeam, polkadot, rococo, westend } from "./known/mod.ts";
