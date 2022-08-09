1d8794fe5f4a83fad15b60e01d7ad07eb14427b7
Harry Solovay <harrysolovay@gmail.com>
2022-08-09 16:04:49 +0800

    add select to atom

--- examples/those_of_subxt/read_era_rewards.ts
+++ examples/those_of_subxt/read_era_rewards.ts
@@ -1,8 +1,10 @@
 import * as C from "../../mod.ts";
 import * as U from "../../util/mod.ts";
 
-const activeEra = C.readEntry(C.polkadot, "Staking", "ActiveEra", []);
-const activeEraIndex = C.select(C.select(activeEra, "value"), "index");
-const eraRewardPoints = C.readEntry(C.polkadot, "Staking", "ErasRewardPoints", [activeEraIndex]);
+const idx = C
+  .readEntry(C.polkadot, "Staking", "ActiveEra", [])
+  .select("value")
+  .select("index");
+const eraRewardPoints = C.readEntry(C.polkadot, "Staking", "ErasRewardPoints", [idx]);
 
 console.log(U.throwIfError(await eraRewardPoints.run()));
