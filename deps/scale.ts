a8a366e0f675b7b2e6ba1ef0c7c0f09ac5e7af0e
T6 <t6@t6.fyi>
2022-08-17 23:23:16 -0400

    Update deps/scale.ts

--- deps/scale.ts
+++ deps/scale.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/paritytech/parity-scale-codec-ts/3b02d9e1e15a1fdfef0fdc8e62cd746afbc534e5/mod.ts";
+export * from "https://deno.land/x/scale@v0.4.2/mod.ts";
