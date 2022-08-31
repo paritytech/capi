d21c31d6ed8c32b334ed3bcb8f67c070960ac87c
Matias Volpe <kratico@users.noreply.github.com>
2022-08-31 18:48:00 -0300

    tighten ss58-js tests (#198)
    Co-authored-by: tjjfvi <t6@t6.fyi>

--- ss58-js/mod.test.ts
+++ ss58-js/mod.test.ts
@@ -11,122 +11,54 @@ import {
   InvalidPublicKeyLengthError,
 } from "./mod.ts";
 
-(<[networkName: string, prefix: number, publicKey: Uint8Array, base58EncodedAddress: string][]> [
-  [
-    "Polkadot",
-    0,
-    p.alice.publicKey,
-    "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5",
-  ],
-  [
-    "Substrate",
-    42,
-    p.alice.publicKey,
-    "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-  ],
-  [
-    "Aventus",
-    65,
-    p.alice.publicKey,
-    "cLxkfNUiCYsb57YLhTJdNVKxUTB1VTpeygYZNhYuFc83KrFy7", // cspell:disable-line
-  ],
-])
-  .forEach(
-    ([networkName, prefix, publicKey, base58EncodedAddress]) => {
-      Deno.test({
-        name: `ss58.encode should encode a ${networkName} address with ${prefix} prefix`,
-        fn: () => {
-          const actual = encode(prefix, publicKey);
-
-          assertEquals(actual, base58.decode(base58EncodedAddress));
-        },
-      });
-    },
-  );
-
-(<[
-  testSpec: string,
-  prefix: number,
-  publicKey: Uint8Array,
-  validNetworkPrefixes: number[] | undefined,
-  ErrorClass: ErrorConstructor,
-][]> [
-  [
-    "should throw for an invalid public key length",
-    0,
-    p.alice.publicKey.slice(0, 30),
-    undefined,
-    InvalidPublicKeyLengthError,
-  ],
-  [
-    "should throw for an invalid network prefix",
-    46,
-    p.alice.publicKey,
-    [0],
-    InvalidNetworkPrefixError,
-  ],
-]).forEach(([testSpec, prefix, publicKey, validNetworkPrefixes, ErrorClass]) => {
-  Deno.test({
-    name: `ss58.encode ${testSpec}`,
-    fn: () => {
-      assertThrows(() => encode(prefix, publicKey, validNetworkPrefixes), ErrorClass);
-    },
+for (
+  const [networkName, address, [prefix, publicKey]] of [
+    [
+      "polkadot",
+      "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5",
+      [0, p.alice.publicKey],
+    ],
+    [
+      "substrate",
+      "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+      [42, p.alice.publicKey],
+    ],
+    [
+      "aventus",
+      "cLxkfNUiCYsb57YLhTJdNVKxUTB1VTpeygYZNhYuFc83KrFy7", // cspell:disable-line
+      [65, p.alice.publicKey],
+    ],
+  ] as const
+) {
+  Deno.test(`ss58.encode ${networkName}`, () => {
+    const actual = base58.encode(encode(prefix, publicKey));
+    assertEquals(actual, address);
   });
+  Deno.test(`ss58.decode ${networkName}`, () => {
+    const actual = decode(base58.decode(address));
+    assertEquals(actual, [prefix, publicKey]);
+  });
+}
+
+Deno.test("ss58.encode invalid public key length", () => {
+  assertThrows(() => encode(0, p.alice.publicKey.slice(0, 30)), InvalidPublicKeyLengthError);
 });
 
-(<[
-  networkName: string,
-  base58EncodedAddress: string,
-  decodedAddress: [prefix: number, publicKey: Uint8Array],
-][]> [
-  [
-    "Polkadot",
-    "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5",
-    [0, p.alice.publicKey],
-  ],
-  [
-    "Substrate",
-    "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-    [42, p.alice.publicKey],
-  ],
-  [
-    "Aventus",
-    "cLxkfNUiCYsb57YLhTJdNVKxUTB1VTpeygYZNhYuFc83KrFy7", // cspell:disable-line
-    [65, p.alice.publicKey],
-  ],
-]).forEach(([networkName, base58EncodedAddress, [prefix, publicKey]]) => {
-  Deno.test({
-    name: `ss58.decode should decode a ${networkName} address`,
-    fn: () => {
-      const actual = decode(base58.decode(base58EncodedAddress));
-
-      assertEquals(actual, [prefix, publicKey]);
-    },
-  });
+Deno.test("ss58.encode invalid network prefix", () => {
+  assertThrows(() => encode(46, p.alice.publicKey, [0]), InvalidNetworkPrefixError);
 });
 
-(<[name: string, base58EncodedAddress: string, ErrorClass: ErrorConstructor][]> [
-  [
-    "should throw for a long address",
-    "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5" + "11111111",
-    InvalidAddressLengthError,
-  ],
-  [
-    "should throw for a short address",
-    "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5".slice(0, 40),
-    InvalidAddressLengthError,
-  ],
-  [
-    "should throw for an invalid checksum",
-    // Alice public key with a 0xff 0xff checksum
-    "15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6ccE",
+Deno.test("ss58.decode long address", () => {
+  assertThrows(() => decode(new Uint8Array(40)), InvalidAddressLengthError);
+});
+
+Deno.test("ss58.decode short address", () => {
+  assertThrows(() => decode(new Uint8Array(30)), InvalidAddressLengthError);
+});
+
+Deno.test("ss58.decode invalid checksum", () => {
+  assertThrows(
+    () => decode(Uint8Array.of(0, ...p.alice.publicKey, 255, 255)),
     InvalidAddressChecksumError,
-  ],
-]).forEach(([testSpec, base58EncodedAddress, ErrorClass]) => {
-  Deno.test({
-    name: `ss58.decode ${testSpec}`,
-    fn: () => {
-      assertThrows(() => decode(base58.decode(base58EncodedAddress)), ErrorClass);
-    },
-  });
+  );
 });
