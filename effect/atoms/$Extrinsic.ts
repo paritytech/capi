9f3be8b2126961b14678fbc908df0f545c1bd1fe
tjjfvi <t6@t6.fyi>
2022-09-01 19:05:10 -0400

    add js hasher impls

--- effect/atoms/$Extrinsic.ts
+++ effect/atoms/$Extrinsic.ts
@@ -1,15 +1,13 @@
 import * as M from "../../frame_metadata/mod.ts";
-import { Hashers } from "../../hashers/mod.ts";
 import { atomFactory } from "../sys/Atom.ts";
 
-export const $extrinsic = atomFactory("ExtrinsicCodec", async (
+export const $extrinsic = atomFactory("ExtrinsicCodec", (
   deriveCodec: M.DeriveCodec,
   metadata: M.Metadata,
   sign?: M.SignExtrinsic,
 ) => {
   return M.$extrinsic({
     deriveCodec,
-    hashers: await Hashers(),
     metadata,
     sign: sign!,
   });
--- effect/atoms/$Key.ts
+++ effect/atoms/$Key.ts
@@ -1,15 +1,13 @@
 import * as M from "../../frame_metadata/mod.ts";
-import { Hashers } from "../../hashers/mod.ts";
 import { atomFactory } from "../sys/Atom.ts";
 
-export const $key = atomFactory("KeyCodec", async (
+export const $key = atomFactory("KeyCodec", (
   deriveCodec: M.DeriveCodec,
   pallet: M.Pallet,
   storageEntry: M.StorageEntry,
 ) => {
   return M.$storageKey({
     deriveCodec,
-    hashers: await Hashers(),
     pallet,
     storageEntry,
   });
