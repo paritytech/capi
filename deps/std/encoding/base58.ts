d702a0cf8ac5d4fd6549795752119d1cad2ec12f
Matias Volpe <kratico@users.noreply.github.com>
2022-09-02 08:55:20 -0300

    Update deps/std/encoding/base58.ts
    Co-authored-by: Harry Solovay <harrysolovay@gmail.com>

--- deps/std/encoding/base58.ts
+++ deps/std/encoding/base58.ts
@@ -1,3 +1,3 @@
 // FIXME: update dependency when deno release base58 encoding
 // export * from "https://deno.land/std@0.xyz.0/encoding/base58.ts";
-export * from "https://raw.githubusercontent.com/denoland/deno_std/main/encoding/base58.ts";
+export * from "https://deno.land/std@0.154.0/encoding/base58.ts";
