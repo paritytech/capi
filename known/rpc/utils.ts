12d7d208de649ca1153139f4492c4d61f51eedd3
tjjfvi <t6@t6.fyi>
2022-09-17 09:46:09 -0700

    ...

---
+++ known/rpc/utils.ts
@@ -0,0 +1,16 @@
+import { Expand } from "../../deps/scale.ts";
+import * as U from "../../util/mod.ts";
+
+export type SerdeResult<O, E> = SerdeEnum<{ Ok: O; Err: E }>;
+export type SerdeEnum<T> = {
+  [K in keyof T]: T[K] extends void ? K : Expand<Pick<T, K> & Omit<{ [K in keyof T]?: never }, K>>;
+}[keyof T];
+
+export type Hex = U.Hex;
+export type Hash = U.HexHash;
+export type SubId = string;
+export type AccId = string;
+export type Subscription<T, U> = null;
+export type NumberOrHex = U.HexEncoded<bigint> | number;
+export type ListOrValue<T> = T | T[];
+export type Result<T> = T;
