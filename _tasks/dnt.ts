acbb3ec25e8920e53ec5cf6aaaace14f4f29008d
T6 <t6@t6.fyi>
2022-09-27 09:25:48 -0700

    fix: watch blocks (#246)

--- _tasks/dnt.ts
+++ _tasks/dnt.ts
@@ -23,9 +23,9 @@ await Promise.all([
     }],
     outDir,
     mappings: {
-      "https://deno.land/x/scale@v0.5.0/mod.ts": {
+      "https://deno.land/x/scale@v0.5.3/mod.ts": {
         name: "parity-scale-codec",
-        version: "^0.5.0",
+        version: "^0.5.3",
       },
       "deps/smoldot_phantom.ts": {
         name: "@substrate/smoldot-light",
--- deps/scale.ts
+++ deps/scale.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/scale@v0.5.0/mod.ts";
+export * from "https://deno.land/x/scale@v0.5.3/mod.ts";
