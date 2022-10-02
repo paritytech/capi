3e1b917814dee593296d07da534c38b996d5d22e
Harry Solovay <harrysolovay@gmail.com>
2022-10-03 01:13:53 +0700

    chore: remove FRAME type placeholder from config (#252)

--- config/mod.ts
+++ config/mod.ts
@@ -1,5 +1,4 @@
 import * as rpc from "../rpc/mod.ts";
-import type * as M from "./metadata.ts";
 
 /** We represent as a class, not a branded type, because we want to extend into a pretty signature. */
 export class Config<
@@ -7,38 +6,14 @@ export class Config<
   RpcCallMethods extends rpc.ProviderMethods = rpc.ProviderMethods,
   RpcSubscriptionMethods extends rpc.ProviderMethods = rpc.ProviderMethods,
   RpcErrorDetails extends rpc.ErrorDetails = rpc.ErrorDetails,
-  Metadata extends M.Metadata = M.Metadata,
 > {
   RpcMethods!: RpcCallMethods & RpcSubscriptionMethods;
   RpcCallMethods!: RpcCallMethods;
   RpcSubscriptionMethods!: RpcSubscriptionMethods;
   RpcErrorDetails!: RpcErrorDetails;
-  Metadata!: Metadata;
 
   constructor(
     readonly discoveryValue: DiscoveryValue,
     readonly addressPrefix: number,
   ) {}
-}
-
-export namespace Config {
-  export function from<
-    RpcMethods extends rpc.ProviderMethods,
-    RpcSubscriptionMethods extends rpc.ProviderMethods,
-    RpcErrorDetails extends rpc.ErrorDetails,
-    Metadata extends M.Metadata,
-  >() {
-    return <D>(
-      discoveryValue: D,
-      addressPrefix: number,
-    ) => {
-      return class
-        extends Config<D, RpcMethods, RpcSubscriptionMethods, RpcErrorDetails, Metadata>
-      {
-        constructor() {
-          super(discoveryValue, addressPrefix);
-        }
-      };
-    };
-  }
 }
--- known/mod.ts
+++ known/mod.ts
@@ -1,5 +1,4 @@
 export * from "./configs.ts";
 export * from "./generated.ts";
-export * from "./metadata.ts";
 export * as rpc from "./rpc.ts";
 export * as types from "./types/mod.ts";
