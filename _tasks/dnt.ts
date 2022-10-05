a4606f1d8bb9effe16d9bcc6758c739475614d86
tjjfvi <t6@t6.fyi>
2022-10-05 10:30:13 -0700

    scale v0.6.0

--- _tasks/dnt.ts
+++ _tasks/dnt.ts
@@ -23,9 +23,9 @@ await Promise.all([
     }],
     outDir,
     mappings: {
-      "https://deno.land/x/scale@v0.5.3/mod.ts": {
+      "https://deno.land/x/scale@v0.6.0/mod.ts": {
         name: "parity-scale-codec",
-        version: "^0.5.3",
+        version: "^0.6.0",
       },
       "deps/smoldot_phantom.ts": {
         name: "@substrate/smoldot-light",
