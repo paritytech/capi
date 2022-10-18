355faae1523ee173534dbd47fee5511abb9be0d4
Matias Volpe <matias@parity.io>
2022-10-18 13:23:49 -0300

    fix: exampls/ticker.ts

--- examples/ticker.ts
+++ examples/ticker.ts
@@ -2,11 +2,14 @@ import * as C from "../mod.ts";
 import * as T from "../test_util/mod.ts";
 import * as U from "../util/mod.ts";
 
-const root = C.watchEntry(T.polkadot, "Timestamp", "Now", [], () => {
+const root = C.watchEntry(T.polkadot, "Timestamp", "Now", [], (stop) => {
   let i = 0;
   return (m) => {
-    console.log({ [i]: m });
     i++;
+    console.log({ [i]: m });
+    if (i === 5) {
+      stop();
+    }
   };
 });
 
