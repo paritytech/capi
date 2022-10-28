a6425f0e6c4a4ccd33ce7f9092d7760341504718
Matias Volpe <matias@parity.io>
2022-10-28 13:14:55 +0200

    feat: add rpc subscriptions

--- rpc_new/client.test.ts
+++ rpc_new/client.test.ts
@@ -1,3 +1,4 @@
+import { delay } from "../deps/std/async.ts";
 import { polkadot } from "../test_util/config.ts";
 import { Client } from "./client.ts";
 import { proxyProvider } from "./provider/proxy.ts";
@@ -9,5 +10,38 @@ const result = await client.call({
   method: "state_getMetadata",
   params: [],
 });
-await client.close();
 console.log(result);
+
+// client.subscribe(
+//   {
+//     jsonrpc: "2.0",
+//     id: "1",
+//     method: "chain_subscribeNewHead",
+//     params: [],
+//   },
+//   (message) => {
+//     console.log(message);
+//   },
+// );
+
+client.subscribeWithStop(
+  {
+    jsonrpc: "2.0",
+    id: "1",
+    method: "chain_subscribeNewHead",
+    params: [],
+  },
+  (stop) => {
+    let i = 0;
+    return (message) => {
+      console.log(message);
+      if (++i === 3) {
+        stop();
+      }
+    };
+  },
+);
+
+await delay(30000);
+
+await client.close();
