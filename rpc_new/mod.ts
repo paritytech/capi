9a13dec3b38471d7d800512efc362aabb464f54c
Harry Solovay <harrysolovay@gmail.com>
2022-10-30 06:03:34 -0400

    export provider error ctors

--- rpc_new/mod.ts
+++ rpc_new/mod.ts
@@ -2,6 +2,7 @@ export * from "./client.ts";
 export * from "./messages.ts";
 export * from "./narrow.ts";
 export * from "./provider/base.ts";
+export * from "./provider/errors.ts";
 export * from "./provider/proxy.ts";
 export * from "./provider/smoldot.ts";
 export * from "./util.ts";
