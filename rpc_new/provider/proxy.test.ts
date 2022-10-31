9c262199c7afa95941868e950293c5a3c4584142
Matias Volpe <matias@parity.io>
2022-10-31 10:09:23 +0100

    feat: add proxy provider unit test

--- rpc_new/provider/proxy.test.ts
+++ rpc_new/provider/proxy.test.ts
@@ -1,16 +1,28 @@
+import { deferred } from "../../deps/std/async.ts";
+import { assertExists, assertNotInstanceOf } from "../../deps/std/testing/asserts.ts";
 import * as T from "../../test_util/mod.ts";
 import { proxyProvider } from "./proxy.ts";
 
-const url = await T.polkadot.initDiscoveryValue();
-
-const provider = proxyProvider(url, async (message) => {
-  console.log(message);
-  await provider.release();
-});
-
-provider.send({
-  jsonrpc: "2.0",
-  id: "0",
-  method: "system_health",
-  params: [],
+Deno.test({
+  name: "Proxy Provider",
+  async fn() {
+    const stopped = deferred();
+    const provider = await proxyProvider(
+      await T.polkadot.initDiscoveryValue(),
+      (message) => {
+        assertNotInstanceOf(message, Error);
+        assertExists(message.result);
+        stopped.resolve();
+      },
+    );
+    provider.send({
+      jsonrpc: "2.0",
+      id: provider.nextId(),
+      method: "system_health",
+      params: [],
+    });
+    await stopped;
+    const providerRelease = await provider.release();
+    assertNotInstanceOf(providerRelease, Error);
+  },
 });
