c89d913b29adf96b068eef9b1da6ed7fc84bb1b3
Matias Volpe <matias@parity.io>
2022-10-31 10:48:19 +0100

    feat: add rpc client unit test

--- rpc_new/client.test.ts
+++ rpc_new/client.test.ts
@@ -1,45 +1,68 @@
+import { deferred } from "../deps/std/async.ts";
+import {
+  assert,
+  assertEquals,
+  assertExists,
+  assertNotInstanceOf,
+} from "../deps/std/testing/asserts.ts";
 import * as known from "../known/rpc/mod.ts";
-import { polkadot } from "../test_util/config.ts";
+import * as T from "../test_util/mod.ts";
+import * as msg from "./messages.ts";
 import { Client, proxyProvider } from "./mod.ts";
 
-const client = new Client(proxyProvider, await polkadot.initDiscoveryValue());
+Deno.test({
+  name: "RPC Client",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn(t) {
+    const client = new Client(proxyProvider, await T.polkadot.initDiscoveryValue());
 
-const metadata = await client.call({
-  jsonrpc: "2.0",
-  id: client.providerRef.nextId(),
-  method: "state_getMetadata",
-  params: [],
-});
+    await t.step({
+      name: "call",
+      async fn() {
+        const metadata = await client.call({
+          jsonrpc: "2.0",
+          id: client.providerRef.nextId(),
+          method: "state_getMetadata",
+          params: [],
+        });
+        assertNotInstanceOf(metadata, Error);
+        assert(!metadata.error);
+        assertExists(metadata.result);
+      },
+    });
 
-if (metadata instanceof Error) {
-  throw metadata;
-} else if (metadata.error) {
-  console.error(metadata.error);
-} else {
-  console.log(metadata.result);
-}
+    await t.step({
+      name: "subscribe",
+      async fn() {
+        const stopped = deferred();
+        const events: msg.NotificationMessage<"chain_subscribeAllHeads", known.Header>[] = [];
+        class Counter {
+          i = 0;
+        }
+        client.subscribe<"chain_subscribeAllHeads", known.Header>({
+          jsonrpc: "2.0",
+          id: client.providerRef.nextId(),
+          method: "chain_subscribeAllHeads",
+          params: [],
+        }, function(event) {
+          const counter = this.state(Counter);
+          assertNotInstanceOf(event, Error);
+          assert(!event.error);
+          assertExists(event.params.result.parentHash);
+          events.push(event);
+          if (counter.i === 2) {
+            this.stop();
+            stopped.resolve();
+            return;
+          }
+          counter.i++;
+        });
+        await stopped;
+        assertEquals(events.length, 3);
+      },
+    });
 
-class Counter {
-  i = 0;
-}
-
-client.subscribe<"chain_subscribeAllHeads", known.Header>({
-  jsonrpc: "2.0",
-  id: client.providerRef.nextId(),
-  method: "chain_subscribeAllHeads",
-  params: [],
-}, async function(event) {
-  const counter = this.state(Counter);
-  if (event instanceof Error) {
-    throw event;
-  } else if (event.error) {
-    console.error(event.error);
-  } else {
-    console.log(event.params.result);
-  }
-  if (counter.i === 4) {
-    this.stop();
     await client.discard();
-  }
-  counter.i += 1;
+  },
 });
