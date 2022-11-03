bc0fe3b374ba597f677c5a92ec210406840e3596
Matias Volpe <matias@parity.io>
2022-11-03 12:40:18 +0100

    feat: update smoldot unit test

--- rpc_new/provider/smoldot.test.ts
+++ rpc_new/provider/smoldot.test.ts
@@ -1,23 +1,31 @@
 // TODO
+import { deferred } from "../../deps/std/async.ts";
+import { assertExists, assertNotInstanceOf } from "../../deps/std/testing/asserts.ts";
 import { smoldotProvider } from "./smoldot.ts";
 
 const POLKADOT_CHAIN_SPEC_URL =
-  "https://raw.githubusercontent.com/paritytech/smoldot/main/bin/polkadot.json";
+  "https://raw.githubusercontent.com/paritytech/substrate-connect/main/packages/connect/src/connector/specs/polkadot.json";
 
 Deno.test({
   name: "Smoldot Provider",
   ignore: true,
   async fn() {
+    const stopped = deferred();
     const polkadotChainSpec = await (await fetch(POLKADOT_CHAIN_SPEC_URL)).text();
     const provider = smoldotProvider(polkadotChainSpec, (message) => {
+      assertNotInstanceOf(message, Error);
+      assertExists(message.result);
       console.log(message);
-      provider.release();
+      stopped.resolve();
     });
     provider.send({
       jsonrpc: "2.0",
       id: provider.nextId(),
-      method: "state_getMetadata",
+      method: "system_health",
       params: [],
     });
+
+    await stopped;
+    await provider.release();
   },
 });
