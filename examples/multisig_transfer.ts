0245794c757bfb36b8a33f725e335cf49a25277c
Harry Solovay <harrysolovay@gmail.com>
2022-11-09 09:28:59 -0500

    debugging zones usage

--- examples/multisig_transfer.ts
+++ examples/multisig_transfer.ts
@@ -1,14 +1,14 @@
+import { createKeyMulti } from "../deps/polkadot/util-crypto.ts";
+import * as C from "../mod.ts";
+import * as T from "../test_util/mod.ts";
+import * as U from "../util/mod.ts";
+
 // FIXME: remove this check once the Zones .bind(env) fix is merged
 const hostname = Deno.env.get("TEST_CTX_HOSTNAME");
 const portRaw = Deno.env.get("TEST_CTX_PORT");
 if (!hostname || !portRaw) {
   throw new Error("Must be running inside a test ctx");
 }
-
-import { createKeyMulti } from "../deps/polkadot/util-crypto.ts";
-import * as C from "../mod.ts";
-import * as T from "../test_util/mod.ts";
-import * as U from "../util/mod.ts";
 
 // Signatories must be sorted
 const signatories: Uint8Array[] = [
@@ -19,23 +19,44 @@ const signatories: Uint8Array[] = [
 const THRESHOLD = 2;
 const multisigPublicKey = createKeyMulti(signatories, THRESHOLD);
 
-const env = C.Z.env();
-
 // Transfer initial balance (existential deposit) to multisig address
-U.throwIfError(await addBalanceToMultisigAddress().bind(env)());
+const existentialDeposit = C.extrinsic({
+  client: T.polkadot,
+  sender: {
+    type: "Id",
+    value: T.alice.publicKey,
+  },
+  palletName: "Balances",
+  methodName: "transfer",
+  args: {
+    value: 2_000_000_000_000n,
+    dest: {
+      type: "Id",
+      value: multisigPublicKey,
+    },
+  },
+})
+  .signed((message) => ({
+    type: "Sr25519",
+    value: T.alice.sign(message),
+  }))
+  .watch(function(status) {
+    console.log(status);
+    if (C.TransactionStatus.isTerminal(status)) {
+      this.stop();
+    }
+  });
 
-// Create proposal
-U.throwIfError(
-  await createOrApproveMultisigProposal({
-    publicKey: T.alice.publicKey,
-    sign: C.Z.ls().next(() =>
-      (message) => ({
-        type: "Sr25519",
-        value: T.alice.sign(message),
-      })
-    ),
-  }).bind(env)(),
-);
+// First approval root
+const proposal = createOrApproveMultisigProposal({
+  publicKey: T.alice.publicKey,
+  sign: (message) => {
+    return ({
+      type: "Sr25519",
+      value: T.alice.sign(message),
+    });
+  },
+});
 
 // Get the key of the timepoint
 const key = C.keyPageRead(T.polkadot)("Multisig", "Multisigs", 1, [multisigPublicKey])
@@ -43,77 +64,43 @@ const key = C.keyPageRead(T.polkadot)("Multisig", "Multisigs", 1, [multisigPubli
   .access(1);
 
 // Get the timepoint itself
-const maybeTimepoint = await C.entryRead(T.polkadot)("Multisig", "Multisigs", [
+const maybeTimepoint = C.entryRead(T.polkadot)("Multisig", "Multisigs", [
   multisigPublicKey,
   key,
 ])
   .access("value")
-  .access("when")
-  .bind(env)();
+  .access("when");
 
-U.throwIfError(
-  await createOrApproveMultisigProposal({
-    publicKey: T.bob.publicKey,
-    sign: C.Z.ls().next(() =>
-      (message) => ({
-        type: "Sr25519",
-        value: T.bob.sign(message),
-      })
-    ),
-    maybeTimepoint,
-  }).bind(env)(),
-);
+const approval = createOrApproveMultisigProposal({
+  publicKey: T.bob.publicKey,
+  sign: (message) => ({
+    type: "Sr25519",
+    value: T.bob.sign(message),
+  }),
+  maybeTimepoint,
+});
 
 // check T.dave new balance
-console.log(
-  U.throwIfError(
-    await C.entryRead(T.polkadot)("System", "Account", [T.dave.publicKey]).bind(env)(),
-  ),
-);
+const daveBalance = C.entryRead(T.polkadot)("System", "Account", [T.dave.publicKey]);
 
-function addBalanceToMultisigAddress() {
-  return C.extrinsic({
-    client: T.polkadot,
-    sender: {
-      type: "Id",
-      value: T.alice.publicKey,
-    },
-    palletName: "Balances",
-    methodName: "transfer",
-    args: {
-      value: 2_000_000_000_000n,
-      dest: {
-        type: "Id",
-        value: multisigPublicKey,
-      },
-    },
-  })
-    .signed((message) => ({
-      type: "Sr25519",
-      value: T.alice.sign(message),
-    }))
-    .watch(function(status) {
-      console.log(status);
-      if (C.TransactionStatus.isTerminal(status)) {
-        this.stop();
-      }
-    });
-}
+const env = C.Z.env();
+U.throwIfError(await existentialDeposit.bind(env)());
+U.throwIfError(await proposal.bind(env)());
+U.throwIfError(await approval.bind(env)());
+// U.throwIfError(await daveBalance.bind(env)());
 
-function createOrApproveMultisigProposal(
-  {
-    publicKey,
-    sign,
-    maybeTimepoint,
-  }: {
-    publicKey: Uint8Array;
-    sign: C.Z.$<C.M.SignExtrinsic>;
-    maybeTimepoint?: {
-      height: number;
-      index: number;
-    };
-  },
-) {
+function createOrApproveMultisigProposal({
+  publicKey,
+  sign,
+  maybeTimepoint,
+}: {
+  publicKey: Uint8Array;
+  sign: C.Z.$<C.M.SignExtrinsic>;
+  maybeTimepoint?: C.Z.$<{
+    height: number;
+    index: number;
+  }>;
+}) {
   const createEx = (weight?: bigint) => {
     return C.extrinsic({
       client: T.polkadot,
@@ -123,7 +110,7 @@ function createOrApproveMultisigProposal(
       },
       palletName: "Multisig",
       methodName: "as_multi",
-      args: {
+      args: C.Z.rec({
         threshold: THRESHOLD,
         call: {
           type: "Balances",
@@ -140,12 +127,14 @@ function createOrApproveMultisigProposal(
           .filter((value) => value !== publicKey),
         store_call: false,
         // FIXME: use RPC payment.queryInfo(extrinsic, atBlockHash)
-        max_weight: weight || {
-          ref_time: 500_000_000n,
-          proof_size: 0,
-        },
+        // TODO: versions differ between versions of polkadot (will be resolved by using payment query info result)
+        max_weight: 500_000_000n,
+        // max_weight: weight || {
+        //   ref_time: 500_000_000n,
+        //   // proof_size: 0,
+        // },
         maybe_timepoint: maybeTimepoint,
-      },
+      }),
     })
       .signed(sign);
   };
@@ -156,10 +145,11 @@ function createOrApproveMultisigProposal(
   // C.Z.call(weight, (d) => {
   //   console.log("queryInfo", d);
   // }).bind(env)();
-  return createEx().watch(function(status) {
-    console.log(status);
-    if (C.TransactionStatus.isTerminal(status)) {
-      this.stop();
-    }
-  });
+  return createEx()
+    .watch(function(status) {
+      console.log(status);
+      if (C.TransactionStatus.isTerminal(status)) {
+        this.stop();
+      }
+    });
 }
