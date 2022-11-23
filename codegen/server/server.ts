2acba6bcaf6fddd209a1f6f7109d39811fb9da4e
Matias Volpe <kratico@users.noreply.github.com>
2022-11-23 12:23:16 -0300

    fix: codegen rpc.Client setup (#430)
    Co-authored-by: T6 <t6@t6.fyi>

--- codegen/server/server.ts
+++ codegen/server/server.ts
@@ -152,7 +152,7 @@ export const client = new LocalClientEffect(${JSON.stringify(chainUrl.slice(4))}
           `
           : `
 import * as C from ${JSON.stringify(`/@${version}/mod.ts`)}
-export const client = C.rpc.rpcClient(C.rpc.proxyProvider, ${JSON.stringify(chainUrl)})
+export const client = C.rpcClient(C.rpc.proxyProvider, ${JSON.stringify(chainUrl)})
           `,
         importSpecifier: `/@${version}/mod.ts`,
       })
