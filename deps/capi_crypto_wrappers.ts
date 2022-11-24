dc4700ae8ee489afa82dbe5df5430da81c0b5a79
tjjfvi <t6@t6.fyi>
2022-11-24 09:27:05 -0500

    update capi_crypto_wrappers

--- deps/capi_crypto_wrappers.ts
+++ deps/capi_crypto_wrappers.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/paritytech/capi-crypto-wrappers/e15ce9d65afeb479ef095d4ae8b387c4adce3d3f/mod.ts"
+export * from "https://raw.githubusercontent.com/paritytech/capi-crypto-wrappers/14289c5/mod.ts"
