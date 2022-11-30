2bd1af06d53f76e870620ee29ac3355ebe538482
tjjfvi <t6@t6.fyi>
2022-11-30 10:41:00 +0000

    misc multi-value future combinators

--- rune/future.test.ts
+++ rune/future.test.ts
@@ -3,9 +3,54 @@ import { Future } from "./future.ts"
 import { Id } from "./id.ts"
 
 Deno.test("constant", async () => {
-  assertEquals(await Future.constant(123).run(), 123)
+  assertEquals(
+    await Future
+      .constant(123)
+      .run(),
+    123,
+  )
 })
 
 Deno.test("mapValue", async () => {
-  assertEquals(await Future.constant(1).mapValue(Id.loc``, (x) => x + 1).run(), 2)
+  assertEquals(
+    await Future
+      .constant(1)
+      .mapValue(Id.loc``, (x) => x + 1)
+      .run(),
+    2,
+  )
+  assertEquals(
+    await Future
+      .constant(new Error())
+      .mapValue(Id.loc``, () => 123)
+      .run(),
+    new Error(),
+  )
+})
+
+Deno.test("iter", async () => {
+  assertEquals(
+    await Future
+      .constant([1, 2, 3])
+      .iter()
+      .run(),
+    1,
+  )
+  assertEquals(
+    await Future
+      .constant([1, 2, 3])
+      .iter()
+      .skip(2)
+      .run(),
+    3,
+  )
+  assertEquals(
+    await Future
+      .constant([1, 2, 3])
+      .iter()
+      .mapValue(Id.loc``, (x) => x + "")
+      .skip(1)
+      .run(),
+    "2",
+  )
 })
--- rune/future.ts
+++ rune/future.ts
@@ -29,6 +29,7 @@ export abstract class _Future<T, E extends Error> {
     signal.addEventListener("abort", () => this.listeners.delete(cb))
   }
 
+  i = 0
   push(value: T | E) {
     for (const listener of this.listeners) {
       try {
@@ -37,6 +38,7 @@ export abstract class _Future<T, E extends Error> {
         console.error("BUG: Unhandled listener error:", e)
       }
     }
+    this.i++
   }
 
   stop() {
@@ -56,11 +58,10 @@ export class Future<T, E extends Error> {
   private constructor(readonly id: Id, readonly prime: (ctx: FutureCtx) => _Future<T, E>) {}
 
   static new<T, E extends Error, A extends unknown[]>(
-    id: Id,
     ctor: new(ctx: FutureCtx, ...args: A) => _Future<T, E>,
     ...args: A
   ) {
-    return new Future(Id.hash(id, ...args), (ctx) => new ctor(ctx, ...args))
+    return new Future(Id.hash(Id.loc``, ctor, ...args), (ctx) => new ctor(ctx, ...args))
   }
 
   async run(): Promise<T | E> {
@@ -85,45 +86,118 @@ export class Future<T, E extends Error> {
   }
 
   static constant<T>(value: T) {
-    return Future.new(Id.hash(Id.loc``, value), _ConstantFuture, value)
+    return Future.new(_ConstantFuture, value)
   }
 
-  mapValue<T, E extends Error, U>(
+  mapValue<R>(
     this: Future<T, E>,
     id: Id,
-    fn: (value: T) => U,
-  ): Future<Exclude<U, Error>, E | Extract<U, Error>> {
-    return Future.new(
-      Id.loc``,
-      _MapValueFuture,
-      id,
-      this,
-      fn,
-    )
+    fn: (value: T) => R,
+  ): Future<Exclude<R, Error>, E | Extract<R, Error>> {
+    return Future.new(_MapValueFuture, id, this, fn)
+  }
+
+  iter<R>(this: Future<Iterable<R>, E>) {
+    return Future.new(_IterFuture, this)
+  }
+
+  skip(skip: number): Future<T, E> {
+    return Future.new(_SkipFuture, this, skip)
+  }
+
+  take(take: number): Future<T, E> {
+    return Future.new(_TakeFuture, this, take)
+  }
+
+  first(): Future<T, E> {
+    return this.take(1)
   }
 }
 
-class _ConstantFuture<T> extends _Future<T, never> {
+class _ConstantFuture<T> extends _Future<Exclude<T, Error>, Extract<T, Error>> {
   constructor(ctx: FutureCtx, readonly value: T) {
     super(ctx)
   }
 
   start(): void {
-    this.push(this.value)
+    this.push(this.value as Exclude<T, Error> | Extract<T, Error>)
     this.stop()
   }
 }
 
-export class _MapValueFuture<T, E extends Error, U>
-  extends _Future<Exclude<U, Error>, E | Extract<U, Error>>
+class _MapValueFuture<T, E extends Error, R>
+  extends _Future<Exclude<R, Error>, E | Extract<R, Error>>
 {
   base
-  constructor(ctx: FutureCtx, _id: Id, base: Future<T, E>, readonly fn: (value: T) => U) {
+  constructor(ctx: FutureCtx, _id: Id, base: Future<T, E>, readonly fn: (value: T) => R) {
     super(ctx)
     this.base = base.prime(ctx)
     this.base.onPush((value) => {
       if (value instanceof Error) this.push(value)
-      else this.push(this.fn(value) as Exclude<U, Error> | Extract<U, Error>)
+      else this.push(this.fn(value) as Exclude<R, Error> | Extract<R, Error>)
+    }, this.abortController.signal)
+    this.base.abortController.signal.addEventListener("abort", () => {
+      this.stop()
+    })
+  }
+
+  start(): void {
+    this.base.start()
+  }
+}
+
+class _IterFuture<R, E extends Error> extends _Future<Exclude<R, Error>, E | Extract<R, Error>> {
+  base
+  constructor(ctx: FutureCtx, base: Future<Iterable<R>, E>) {
+    super(ctx)
+    this.base = base.prime(ctx)
+    this.base.onPush((value) => {
+      if (value instanceof Error) return this.push(value)
+      for (const v of value) {
+        this.push(v as Exclude<R, Error> | Extract<R, Error>)
+      }
+    }, this.abortController.signal)
+    this.base.abortController.signal.addEventListener("abort", () => {
+      this.stop()
+    })
+  }
+
+  start(): void {
+    this.base.start()
+  }
+}
+
+class _SkipFuture<T, E extends Error> extends _Future<T, E> {
+  base
+  constructor(ctx: FutureCtx, base: Future<T, E>, public skip: number) {
+    super(ctx)
+    this.base = base.prime(ctx)
+    this.base.onPush((value) => {
+      if (this.base.i >= skip) {
+        this.push(value)
+      }
+    }, this.abortController.signal)
+    this.base.abortController.signal.addEventListener("abort", () => {
+      this.stop()
+    })
+  }
+
+  start(): void {
+    this.base.start()
+  }
+}
+
+class _TakeFuture<T, E extends Error> extends _Future<T, E> {
+  base
+  constructor(ctx: FutureCtx, base: Future<T, E>, public take: number) {
+    super(ctx)
+    this.base = base.prime(ctx)
+    this.base.onPush((value) => {
+      if (this.base.i < take) {
+        this.push(value)
+      } else {
+        this.stop()
+      }
     }, this.abortController.signal)
     this.base.abortController.signal.addEventListener("abort", () => {
       this.stop()
