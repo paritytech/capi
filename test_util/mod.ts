311e2bfae0854f9907ca77b80bca0b80fe1acba2
Harry Solovay <harrysolovay@gmail.com>
2022-12-21 15:09:14 -0800

    file casing cleanup

--- test_util/mod.ts
+++ test_util/mod.ts
@@ -9,5 +9,5 @@ export {
 export * as extrinsic from "./extrinsic.ts"
 export * from "./local.ts"
 export * from "./pairs.ts"
-export * from "./sr25519.ts"
+export * from "./Sr25519.ts"
 export * as zombienet from "./zombienet.ts"
