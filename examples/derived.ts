9c9729f48b79ddaf42a8dcaab0a88bd169593d0d
Matias Volpe <matias@parity.io>
2023-01-04 15:31:20 -0300

    fix: derived.ts example

--- examples/derived.ts
+++ examples/derived.ts
@@ -1,9 +1,18 @@
 import * as C from "http://localhost:5646/@local/mod.ts"
 import * as U from "http://localhost:5646/@local/util/mod.ts"
 
-const ids = C.entryRead(C.polkadot)("Paras", "Parachains", [])
-  .access("value")
-  .as<number[]>()
+// TODO: uncomment these lines to run a single effect upon solving `count` in zones
+// or effects/rpc.ts#discardCheck
+// const ids = C.entryRead(C.polkadot)("Paras", "Parachains", [])
+//   .access("value")
+//   .as<number[]>()
+
+const ids = U.throwIfError(
+  await C.entryRead(C.polkadot)("Paras", "Parachains", [])
+    .access("value")
+    .as<number[]>()
+    .run(),
+)
 
 const root = C.Z.each(ids, (id) => {
   return C.entryRead(C.polkadot)("Paras", "Heads", [id])
