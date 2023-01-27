c0fd95abdcf401d64c8b5824cfe70f52b85b6be9
Harry Solovay <harrysolovay@gmail.com>
2023-01-27 12:41:10 -0500

    get rid of unwanted console log

--- providers/frame/ProxyBase.ts
+++ providers/frame/ProxyBase.ts
@@ -13,7 +13,6 @@ export abstract class FrameProxyProvider extends FrameProvider {
 
   async client(pathInfo: PathInfo) {
     const url = await this.url(pathInfo)
-    if (this.providerId === "zombienet") console.log(url)
     const client = new Client(proxyProvider, url)
     this.env.signal.addEventListener("abort", client.discard)
     return client
