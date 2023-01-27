a7062156f6a2477c9095aafe4bc7ee78da426442
Harry Solovay <harrysolovay@gmail.com>
2023-01-27 17:34:02 -0500

    Update server/Env.ts
    Co-authored-by: T6 <t6@t6.fyi>

--- server/Env.ts
+++ server/Env.ts
@@ -3,22 +3,17 @@ import { Provider, ProviderFactory } from "./Provider.ts"
 export class Env {
   providers: Record<string, Record<string, Provider>> = {}
 
-  constructor(readonly signal: AbortSignal, providersFactories: ProviderFactory[]) {
-    providersFactories.forEach((f) => {
-      const provider = f(this)
+  constructor(readonly signal: AbortSignal, providerFactories: ProviderFactory[]) {
+    for (const factory of providerFactories) {
+      const provider = factory(this)
       const { generatorId, providerId } = provider
-      const generatorProviders = this.providers[generatorId]
-      if (generatorProviders) {
-        if (generatorProviders[providerId]) {
-          throw new Error(
-            `${generatorId} Provider with \`providerId\` of \`${providerId}\` already initialized`,
-          )
-        }
-        generatorProviders[providerId] = provider
-      } else {
-        this.providers[generatorId] = { [providerId]: provider }
+      const generatorProviders = this.providers[generatorId] ??= {}
+      if (generatorProviders[providerId]) {
+        throw new Error(
+          `${generatorId} Provider with \`providerId\` of \`${providerId}\` already initialized`,
+        )
       }
-      return provider
-    })
+      generatorProviders[providerId] = provider
+    }
   }
 }
