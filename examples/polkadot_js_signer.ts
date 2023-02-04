b4e17684b1edcb807a28b978f421030cb2c9afe1
tjjfvi <t6@t6.fyi>
2023-02-04 11:06:24 -0500

    tweak star task

--- examples/polkadot_js_signer.ts
+++ examples/polkadot_js_signer.ts
@@ -1,31 +1,31 @@
-import * as C from "capi/mod.ts"
-import { Balances } from "polkadot_dev/mod.ts"
+// import * as C from "capi/mod.ts"
+// import { Balances } from "polkadot_dev/mod.ts"
 
-// Dynamic import because type-checking polkadot-js causes OOM
-const { createTestPairs } = await import(`https://deno.land/x/polkadot@0.2.25/keyring/mod.ts`)
-const { TypeRegistry } = await import(`https://deno.land/x/polkadot@0.2.25/types/mod.ts`)
+// // // Dynamic import because type-checking polkadot-js causes OOM
+// // const { createTestPairs } = await import(`https://deno.land/x/polkadot@0.2.25/keyring/mod.ts`)
+// // const { TypeRegistry } = await import(`https://deno.land/x/polkadot@0.2.25/types/mod.ts`)
 
-await Balances
-  .transfer({
-    value: 12345n,
-    dest: C.bob.address,
-  })
-  .signed({
-    sender: {
-      address: C.alice.address,
-      sign: {
-        signPayload(payload) {
-          const tr = new TypeRegistry()
-          tr.setSignedExtensions(payload.signedExtensions)
-          return Promise.resolve(
-            tr.createType("ExtrinsicPayload", payload, { version: payload.version })
-              .sign(createTestPairs().alice!),
-          )
-        },
-      },
-    },
-  })
-  .sent()
-  .logStatus()
-  .finalized()
-  .run()
+// await Balances
+//   .transfer({
+//     value: 12345n,
+//     dest: C.bob.address,
+//   })
+//   .signed({
+//     sender: {
+//       address: C.alice.address,
+//       sign: {
+//         signPayload(payload) {
+//           const tr = new TypeRegistry()
+//           tr.setSignedExtensions(payload.signedExtensions)
+//           return Promise.resolve(
+//             tr.createType("ExtrinsicPayload", payload, { version: payload.version })
+//               .sign(createTestPairs().alice!),
+//           )
+//         },
+//       },
+//     },
+//   })
+//   .sent()
+//   .logStatus()
+//   .finalized()
+//   .run()
