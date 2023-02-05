12ab82c0518c78a01f670ace31def36ba222e208
Harry Solovay <harrysolovay@gmail.com>
2023-02-04 22:58:41 -0500

    rework transfer example

--- examples/transfer.ts
+++ examples/transfer.ts
@@ -1,20 +1,15 @@
 import { alice, bob } from "capi"
 import { Balances } from "westend_dev/mod.ts"
 
-const tx = Balances
-  .transfer({
-    value: 12345n,
-    dest: bob.address,
-  })
-  .signed({ sender: alice })
-
-const txFinalizedBlock = tx
-  .sent()
-  .logStatus()
-  .finalized()
-
-const result = await txFinalizedBlock
-  .txEvents(tx.hex())
-  .run()
-
-console.log(result)
+console.log(
+  await Balances
+    .transfer({
+      value: 12345n,
+      dest: bob.address,
+    })
+    .signed({ sender: alice })
+    .sent()
+    .logStatus()
+    .events()
+    .run(),
+)
