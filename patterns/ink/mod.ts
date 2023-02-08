f345c3052a0260d869b23a42d91f4749f32adf39
Harry Solovay <harrysolovay@gmail.com>
2023-02-08 18:40:18 -0500

    fix moderate usage

--- patterns/ink/mod.ts
+++ patterns/ink/mod.ts
@@ -1,7 +1,6 @@
 // moderate
 
 export * from "./events.ts"
-export * from "./InkMetadata.test.ts"
 export * from "./InkMetadata.ts"
 export * from "./InkMetadataRune.ts"
 export * from "./InkRune.ts"
--- rune/mod.ts
+++ rune/mod.ts
@@ -2,11 +2,8 @@
 
 export * from "./ArrayRune.ts"
 export * from "./FnRune.ts"
-export * from "./Id.test.ts"
 export * from "./Id.ts"
 export * from "./MetaRune.ts"
-export * from "./Rune.test.ts"
 export * from "./Rune.ts"
-export * from "./RunicArgs.test.ts"
 export * from "./Timeline.ts"
 export * from "./ValueRune.ts"
