49ae3e691f15358b545516375fd432d25c21cb36
Harry Solovay <harrysolovay@gmail.com>
2023-02-08 18:39:14 -0500

    fix moderate usage

--- rune/mod.ts
+++ rune/mod.ts
@@ -1,8 +1,12 @@
-// --moderate
+// moderate
 
+export * from "./ArrayRune.ts"
 export * from "./FnRune.ts"
+export * from "./Id.test.ts"
 export * from "./Id.ts"
 export * from "./MetaRune.ts"
+export * from "./Rune.test.ts"
 export * from "./Rune.ts"
+export * from "./RunicArgs.test.ts"
 export * from "./Timeline.ts"
 export * from "./ValueRune.ts"
