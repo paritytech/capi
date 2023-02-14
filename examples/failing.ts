c6d6d8122ca2ebdaa13f40a327dc8f15f9672e20
Harry Solovay <harrysolovay@gmail.com>
2023-02-14 12:56:54 -0500

    get rid of failing repro

--- examples/failing.ts
+++
@@ -1,6 +0,0 @@
-import { alice } from "capi"
-import { System as KSystem } from "kusama_dev/mod.ts"
-import { System as PSystem } from "polkadot_dev/mod.ts"
-
-console.log(await PSystem.Account.entry([alice.publicKey]).run())
-console.log(await KSystem.Account.entry([alice.publicKey]).run())
