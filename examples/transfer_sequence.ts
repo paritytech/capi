9fdcf58921cd0b01827408e81b3d97e2e9b9a0ff
Harry Solovay <harrysolovay@gmail.com>
2023-02-16 09:46:58 -0500

    get example working

--- examples/transfer_sequence.ts
+++ examples/transfer_sequence.ts
@@ -1,36 +1,45 @@
-// import { alice, bob, charlie, dave, Rune, RunicArgs, Sr25519, ValueRune } from "capi"
-// import { Balances, System } from "westend_dev/mod.ts"
+import { alice, bob, charlie, dave, Rune, RunicArgs, Sr25519, ValueRune } from "capi"
+import { Balances, System } from "westend_dev/mod.ts"
+import { upperFirstCase } from "../deps/case.ts"
 
-// await aliceFree()
-//   .log("Alice balance after transfers")
-//   .chain(() =>
-//     Object
-//       .entries({ bob, charlie, dave })
-//       .reduce<ValueRune<any, any>>((acc, args) =>
-//         acc
-//           .chain(() => beforeThenTxThenAfter(...args))
-//           .into(ValueRune), Rune.constant(undefined))
-//   )
-//   .chain(() => aliceFree().log("Alice balance after transfers"))
-//   .run()
+await aliceFree()
+  .log(msg("Alice", "before"))
+  .chain(() =>
+    Object
+      .entries({ bob, charlie, dave })
+      .reduce<ValueRune<any, any>>((acc, args) =>
+        acc
+          .chain(() => beforeThenTxThenAfter(...args))
+          .into(ValueRune), Rune.constant(undefined))
+  )
+  .chain(() => aliceFree().log(msg("Alice", "after")))
+  .run()
 
-// function aliceFree() {
-//   return System.Account.entry([alice.publicKey]).access("data", "free")
-// }
+function msg<X>(...[name, stage]: RunicArgs<X, [name: string, stage: "before" | "after"]>) {
+  return Rune.str`${name} balance ${stage}:`
+}
 
-// function beforeThenTxThenAfter<X>(...args: RunicArgs<X, [name: string, pair: Sr25519]>) {
-//   const [name, pair] = RunicArgs.resolve(args)
-//   const dest = pair.access("address")
-//   const accountKey = Rune.tuple([pair.access("publicKey")])
-//   const tx = Balances
-//     .transfer({ dest, value: 1000123n })
-//     .signed({ sender: alice })
-//     .sent()
-//     .logStatus(Rune.str`transfer to ${name}:`)
-//     .finalized()
-//   const free = () => System.Account.entry(accountKey).access("data", "free")
-//   return free()
-//     .log(Rune.str`${name} balance before transfer:`)
-//     .chain(() => tx)
-//     .chain(() => free().log(Rune.str`${name} balance after transfer:`))
-// }
+function aliceFree() {
+  return System.Account.entry([alice.publicKey]).access("data", "free")
+}
+
+function beforeThenTxThenAfter<X>(...args: RunicArgs<X, [name: string, pair: Sr25519]>) {
+  const pair = Rune.resolve(args[1])
+  const name = Rune.resolve(args[0]).map(upperFirstCase)
+  const free = () =>
+    System.Account.entry(Rune.tuple([pair.access("publicKey")])).access("data", "free")
+  return free()
+    .log(msg(name, "before"))
+    .chain(() =>
+      Balances
+        .transfer({
+          dest: pair.access("address"),
+          value: 1000123n,
+        })
+        .signed({ sender: alice })
+        .sent()
+        .logStatus(Rune.str`Transfer to ${name}:`)
+        .finalized()
+    )
+    .chain(() => free().log(msg(name, "after")))
+}
