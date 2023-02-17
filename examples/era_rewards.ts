0fa5b3a20f84103b54b89a408d3769889408701f
Harry Solovay <harrysolovay@gmail.com>
2023-02-17 17:11:59 -0500

    more tweaks

--- examples/era_rewards.ts
+++ examples/era_rewards.ts
@@ -1,13 +1,10 @@
+import { Rune } from "capi"
 import { Staking } from "westend/mod.ts"
 
 const idx = Staking.ActiveEra.entry([]).access("index")
 
 const result = await Staking.ErasRewardPoints
-  .entry(
-    idx
-      .map((idx) => typeof idx === "number" ? [idx] as [number] : undefined)
-      .unhandle(undefined),
-  )
+  .entry(Rune.tuple([idx.unhandle(undefined)]))
   .unhandle(undefined)
   .rehandle(undefined)
   .run()
