aac3bb73e0f523ba8f147aeac0823b0bb323246c
Harry Solovay <harrysolovay@gmail.com>
2023-02-18 08:22:26 -0500

    identity pallet usage example

---
+++ examples/identity.ts
@@ -0,0 +1,37 @@
+import { alice, Rune } from "capi"
+import { Identity } from "polkadot_dev/mod.ts"
+import { Data, IdentityInfo } from "polkadot_dev/types/pallet_identity/types.ts"
+import * as $ from "../deps/scale.ts"
+
+await Identity
+  .setIdentity({
+    info: IdentityInfo(Rune.rec({
+      display: Data.Raw13($.str.encode("Chev Chelios")),
+      web: Data.None(),
+      email: Data.None(),
+      twitter: Data.None(),
+      riot: Data.None(),
+      image: Data.None(),
+      legal: Data.None(),
+      pgpFingerprint: undefined,
+      additional: Rune.tuple([
+        Rune.tuple([
+          Data.Raw5($.str.encode("good")),
+          Data.Raw6($.str.encode("movie")),
+        ]),
+      ]),
+    })),
+  })
+  .signed({ sender: alice })
+  .sent()
+  .logStatus()
+  .txEvents()
+  .log()
+  .run()
+
+await Identity.IdentityOf
+  .entry([alice.publicKey])
+  .access("info", "display", "value")
+  .map((value) => $.str.decode(value))
+  .log()
+  .run()
