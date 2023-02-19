07654d15afd833236a5f9d90e7b3e6d27ca6e833
Harry Solovay <harrysolovay@gmail.com>
2023-02-19 14:54:09 -0500

    fix formatting

--- patterns/mod.ts
+++ patterns/mod.ts
@@ -1,4 +1,4 @@
 export * as consensus from "./consensus/mod.ts"
 export * as ink from "./ink/mod.ts"
-export * from "./MultisigRune.ts"
 export * from "./multisigAccountId.ts"
+export * from "./MultisigRune.ts"
