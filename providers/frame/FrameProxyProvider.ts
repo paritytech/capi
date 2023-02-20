7894feac03439e4ea02d62162baed0f9973f1e3a
T6 <t6@t6.fyi>
2023-02-20 10:12:43 -0500

    chore: remove version from ws proxy (#611)

--- providers/frame/FrameProxyProvider.ts
+++ providers/frame/FrameProxyProvider.ts
@@ -8,7 +8,7 @@ import { FrameProvider } from "./FrameProvider.ts"
 export abstract class FrameProxyProvider extends FrameProvider {
   override async handle(request: Request, pathInfo: PathInfo): Promise<Response> {
     if (
-      pathInfo.vRuntime && pathInfo.vRuntime !== "latest" && !pathInfo.filePath
+      !pathInfo.vRuntime && !pathInfo.filePath
       && request.headers.get("upgrade") === "websocket"
     ) {
       return this.proxyWs(request, pathInfo)
@@ -52,6 +52,7 @@ export abstract class FrameProxyProvider extends FrameProvider {
     return new URL(
       fromPathInfo({
         ...pathInfo,
+        vRuntime: "",
         filePath: "",
       }),
       this.env.href.replace(/^http/, "ws"),
