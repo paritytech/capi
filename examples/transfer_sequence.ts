43d446d305ff162e6d7b0fda5a49039bbbb17a16
Harry Solovay <harrysolovay@gmail.com>
2023-02-20 18:50:14 -0500

    t6 cleanup

--- examples/transfer_sequence.ts
+++ examples/transfer_sequence.ts
@@ -1,45 +1,36 @@
-import { alice, bob, charlie, dave, Rune, RunicArgs, Sr25519, ValueRune } from "capi"
+import { alice, bob, charlie, Rune, Sr25519 } from "capi"
 import { Balances, System } from "westend_dev/mod.ts"
-import { upperFirstCase } from "../deps/case.ts"
 
-await aliceFree()
-  .log(msg("Alice", "before"))
-  .chain(() =>
-    Object
-      .entries({ bob, charlie, dave })
-      .reduce<ValueRune<any, any>>((acc, args) =>
-        acc
-          .chain(() => beforeThenTxThenAfter(...args))
-          .into(ValueRune), Rune.constant(undefined))
-  )
-  .chain(() => aliceFree().log(msg("Alice", "after")))
+await Rune
+  .chain(balances)
+  .chain(() => transfer("bob", bob))
+  .chain(balances)
+  .chain(() => transfer("charlie", charlie))
+  .chain(balances)
   .run()
 
-function msg<X>(...[name, stage]: RunicArgs<X, [name: string, when: "before" | "after"]>) {
-  return Rune.str`${name} balance ${stage}:`
+function balances() {
+  return Rune
+    .rec({
+      alice: balance(alice),
+      bob: balance(bob),
+      charlie: balance(charlie),
+    })
+    .log("Balances:")
 }
 
-function aliceFree() {
-  return System.Account.entry([alice.publicKey]).access("data", "free")
+function balance(user: Sr25519) {
+  return System.Account.entry([user.publicKey]).access("data", "free")
 }
 
-function beforeThenTxThenAfter<X>(...args: RunicArgs<X, [name: string, pair: Sr25519]>) {
-  const pair = Rune.resolve(args[1])
-  const name = Rune.resolve(args[0]).map(upperFirstCase)
-  const free = () =>
-    System.Account.entry(Rune.tuple([pair.access("publicKey")])).access("data", "free")
-  return free()
-    .log(msg(name, "before"))
-    .chain(() =>
-      Balances
-        .transfer({
-          dest: pair.access("address"),
-          value: 1000123n,
-        })
-        .signed({ sender: alice })
-        .sent()
-        .logStatus(Rune.str`Transfer to ${name}:`)
-        .finalized()
-    )
-    .chain(() => free().log(msg(name, "after")))
+function transfer(name: string, user: Sr25519) {
+  return Balances
+    .transfer({
+      dest: user.address,
+      value: 1000123n,
+    })
+    .signed({ sender: alice })
+    .sent()
+    .logStatus(`Transfer to ${name}:`)
+    .finalized()
 }
