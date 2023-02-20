83799551a1f78719b20bd49d17606f33709d643e
Harry Solovay <harrysolovay@gmail.com>
2023-02-20 14:30:13 -0500

    fix broken examples

--- examples/era_rewards.ts
+++ examples/era_rewards.ts
@@ -5,7 +5,6 @@ const idx = Staking.ActiveEra.entry([]).access("index")
 
 const result = await Staking.ErasRewardPoints
   .entry(Rune.tuple([idx.unhandle(undefined)]))
-  .rehandle(undefined)
   .run()
 
 console.log(result)
