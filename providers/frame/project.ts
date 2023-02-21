09d59de8d8659ba802b5e670b21a0d993b9ba233
Harry Solovay <harrysolovay@gmail.com>
2023-02-20 19:48:15 -0500

    cleanup

--- providers/frame/project.ts
+++ providers/frame/project.ts
@@ -1,5 +1,3 @@
-import * as path from "../../deps/std/path.ts"
-import { PathInfo } from "../../server/mod.ts"
 import { PermanentMemo } from "../../util/mod.ts"
 import { getAvailable, isReady } from "../../util/port.ts"
 import { FrameProxyProvider } from "./FrameProxyProvider.ts"
@@ -8,32 +6,20 @@ export class ProjectProvider extends FrameProxyProvider {
   providerId = "project"
 
   urlMemo = new PermanentMemo<string, string>()
-  dynamicUrl(pathInfo: PathInfo) {
-    const { target } = pathInfo
-    const workspacePath = target.startsWith("~/")
-      ? path.join(Deno.env.get("HOME")!, target.slice(2))
-      : target === "$cwd"
-      ? Deno.cwd()
-      : target
-    return this.urlMemo.run(workspacePath, async () => {
-      const port = await this.cargoRun(workspacePath)
+  dynamicUrl() {
+    return this.urlMemo.run(Deno.cwd(), async () => {
+      const port = getAvailable()
+      const process = Deno.run({
+        cmd: ["cargo", "run", "--release", "--", "--dev", "--ws-port", port.toString()],
+        stdout: "piped",
+        stderr: "piped",
+      })
+      this.env.signal.addEventListener("abort", () => {
+        process.kill()
+        process.close()
+      })
+      await isReady(port)
       return `ws://localhost:${port}`
     })
-  }
-
-  async cargoRun(workspacePath: string) {
-    const port = getAvailable()
-    const process = Deno.run({
-      cmd: ["cargo", "run", "--release", "--", "--dev", "--ws-port", port.toString()],
-      stdout: "piped",
-      stderr: "piped",
-      cwd: workspacePath,
-    })
-    this.env.signal.addEventListener("abort", () => {
-      process.kill()
-      process.close()
-    })
-    await isReady(port)
-    return port
   }
 }
