ac0ad9b9881930963f7f20c6891d3ec648e444b3
Harry Solovay <harrysolovay@gmail.com>
2023-02-20 23:19:51 -0500

    cleanup

--- deps/dprint.ts
+++ deps/dprint.ts
@@ -3,12 +3,11 @@ import { createStreaming } from "https://deno.land/x/dprint@0.2.0/mod.ts"
 import dprintConfig from "../dprint.json" assert { type: "json" }
 import { readableStreamFromReader } from "./std/streams.ts"
 
-const file = await Deno.open("deps/dprint_ts.wasm", { read: true })
-const body = readableStreamFromReader(file)
-const headers = new Headers()
-headers.set("content-type", "application/wasm")
-const response = new Response(body, { status: 200, headers })
-export const tsFormatter = await createStreaming(Promise.resolve(response))
-
+export const tsFormatter = await createStreaming(Promise.resolve(
+  new Response(
+    readableStreamFromReader(await Deno.open("deps/dprint_ts.wasm", { read: true })),
+    { headers: { "Content-Type": "application/wasm" } },
+  ),
+))
 const { indentWidth, lineWidth, typescript: config } = dprintConfig
 tsFormatter.setConfig({ indentWidth, lineWidth }, config)
