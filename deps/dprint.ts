9a1648cfabd54e920c9a66164959ea55a962385b
Harry Solovay <harrysolovay@gmail.com>
2023-02-22 12:10:21 -0500

    fix: enable dprint dep to be used offline (#622)

--- deps/dprint.ts
+++ deps/dprint.ts
@@ -1,10 +1,1 @@
 export * from "https://deno.land/x/dprint@0.2.0/mod.ts"
-import { createStreaming } from "https://deno.land/x/dprint@0.2.0/mod.ts"
-import dprintConfig from "../dprint.json" assert { type: "json" }
-
-const { indentWidth, lineWidth, typescript: config, plugins } = dprintConfig
-
-export const tsFormatter = await createStreaming(
-  fetch(plugins.find((v) => v.startsWith("https://plugins.dprint.dev/typescript-"))!),
-)
-tsFormatter.setConfig({ indentWidth, lineWidth }, config)
