99c2b5c7f9053b0b00e9f1a72f1cd649e7c2074b
Jason Butler <jasonsantiagobutler@gmail.com>
2023-03-07 15:25:21 -0300

    feat: statemine e2e example

--- examples/statemine.ts
+++ examples/statemine.ts
@@ -1,131 +1,123 @@
-import { alice, bob, Chain, ExtrinsicRune, Rune, RunicArgs, Sr25519, ValueRune } from "capi"
-import {
-  CollectionDetails,
-  ItemDetails,
-} from "http://localhost:4646/frame/zombienet/zombienets/statemine.toml/collator/@v0.9.370/types/pallet_uniques/types.ts"
+import { $, alice, bob, MetaRune, Rune, ValueRune } from "capi"
 import { Uniques } from "zombienet/statemine.toml/collator/@latest/mod.ts"
 
-const sendAndDebug =
-  <X>(...[sender, prefix]: RunicArgs<X, [sender: Sr25519, prefix: String]>) =>
-  <U, C extends Chain = Chain>(extrinsic: ExtrinsicRune<U, C>) => {
-    return extrinsic.signed({ sender }).sent().dbgStatus(prefix)
-  }
+const collection = 1
+const item = 1
 
-// Create Collection and Asset
+const createCollection = Uniques
+  .create({
+    collection,
+    admin: alice.address,
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Create Collection:")
+  .finalized()
 
-const COLLECTION_ID = 1
+const $collectionMetadata = $.object($.field("hello", $.str))
 
-const createCollection = Uniques.create({
-  collection: COLLECTION_ID,
-  admin: alice.address,
-})
+const setCollectionMetadata = Uniques
+  .setCollectionMetadata({
+    collection,
+    isFrozen: true,
+    data: $collectionMetadata.encode({ hello: "collection" }),
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Set Collection Metadata:")
+  .finalized()
 
-const setCollectionMetadata = Uniques.setCollectionMetadata({
-  collection: COLLECTION_ID,
-  isFrozen: true, // freeze collections metadata
-  data: new Uint8Array(),
-})
+const mintAsset = Uniques
+  .mint({
+    collection,
+    item,
+    owner: alice.address,
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Mint Asset:")
+  .finalized()
 
-const ASSET_ID = 1
+const setAssetMetadata = Uniques
+  .setMetadata({
+    collection,
+    item,
+    isFrozen: true,
+    data: new Uint8Array(),
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Set Asset Metadata:")
+  .finalized()
 
-const mintAsset = Uniques.mint({
-  collection: COLLECTION_ID,
-  item: ASSET_ID,
-  owner: alice.address,
-})
-
-const setAssetMetadata = Uniques.setMetadata({
-  collection: COLLECTION_ID,
-  item: ASSET_ID,
-  isFrozen: true, // freeze assets metadata
-  data: new Uint8Array(),
-})
-
-const setAssetPrice = Uniques.setPrice({
-  collection: COLLECTION_ID,
-  item: ASSET_ID,
-  price: 1000000n,
-  whitelistedBuyer: undefined,
-})
+const setAssetPrice = Uniques
+  .setPrice({
+    collection,
+    item,
+    price: 1000000n,
+    whitelistedBuyer: undefined,
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Set Asset Price:")
+  .finalized()
 
 // prevent new mints in collection
-const setCollectionMaxSupply = Uniques.setCollectionMaxSupply({
-  collection: COLLECTION_ID,
-  maxSupply: 1,
-})
+const setCollectionMaxSupply = Uniques
+  .setCollectionMaxSupply({
+    collection,
+    maxSupply: 1,
+  })
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Set Collection Max Supply:")
+  .finalized()
 
 const alicePrepareCollection = Rune
-  .chain(() => createCollection.pipe(sendAndDebug(alice, "Create Collection:")))
-  .chain(() => setCollectionMetadata.pipe(sendAndDebug(alice, "Set Collection Metadata:")))
-  .chain(() => mintAsset.pipe(sendAndDebug(alice, "Mint Asset:")))
-  .chain(() => setAssetMetadata.pipe(sendAndDebug(alice, "Set Asset Metadata:")))
-  .chain(() => setAssetPrice.pipe(sendAndDebug(alice, "Set Asset Price:")))
-  .chain(() =>
-    setCollectionMaxSupply.pipe(
-      sendAndDebug(alice, "Set Collection Max Supply:"),
-    )
-  )
+  .chain(() => createCollection)
+  .chain(() => setCollectionMetadata)
+  .chain(() => mintAsset)
+  .chain(() => setAssetMetadata)
+  .chain(() => setAssetPrice)
+  .chain(() => setCollectionMaxSupply)
 
-// List Collections, Assets, and buy first asset from first collection
-
-const getCollections = Uniques.Class.entryPage(50).into(ValueRune)
-
-const firstCollection = <X>(
-  collections: ValueRune<[[number], CollectionDetails][], X>,
-) =>
-  collections.map(([first, ..._rest]) => first?.[0][0]).unhandle(undefined)
-    .into(ValueRune)
-
-// TODO: should use RunicArgs?
-const getCollectionAssets = <X>(collectionId: Rune<number, X>) =>
-  Uniques.Asset.entryPage(50, [collectionId]).into(ValueRune)
-
-const firstAsset = <X>(
-  assets: ValueRune<[[number, number], ItemDetails][], X>,
-) =>
-  assets.map(([first, ..._rest]) => first?.[0][1]).unhandle(undefined).into(
-    ValueRune,
-  )
-
-const getItemPrice = <X>(
-  ...args: RunicArgs<X, [collectionId: number, assetId: number]>
-) => {
-  return Uniques.ItemPriceOf.entry(Rune.tuple(args)).unhandle(undefined).map((
-    [price],
-  ) => price)
-}
-
-const buyAsset = <X>(
-  ...[collectionId, assetId, bidPrice]: RunicArgs<
-    X,
-    [collectionId: number, assetId: number, bidPrice: bigint]
-  >
-) => {
-  return Uniques.buyItem({ collection: collectionId, item: assetId, bidPrice })
-}
-
-const collectionId = getCollections.chain(firstCollection)
-const assetId = getCollectionAssets(collectionId).chain(firstAsset)
-const price = getItemPrice(collectionId, assetId)
-
-const bobBuyAsset = buyAsset(collectionId, assetId, price).pipe(
-  sendAndDebug(bob, "Buying Asset:"),
-)
-
-// Query asset details & verify new owner
-
-// const collectionDetails = Uniques.Class.entry(Rune.tuple([collectionId])).unhandle(undefined);
-
-const assetDetails = Uniques.Asset.entry(Rune.tuple([collectionId, assetId]))
+const collectionId = Uniques.Class
+  .entryPage(50)
+  .into(ValueRune)
+  .access(0, 0, 0)
   .unhandle(undefined)
 
-const showAssetOwner = assetDetails.access("owner").dbg("New Owner:")
+const assetId = collectionId.map((collectionId) => Uniques.Asset.entryPage(50, [collectionId]))
+  .into(MetaRune)
+  .flat()
+  .into(ValueRune)
+  .access(0, 0, 1)
+  .unhandle(undefined)
 
-const program = Rune
+const price = Uniques.ItemPriceOf
+  .entry(Rune.tuple([collectionId, assetId]))
+  .unhandle(undefined)
+  .access(0)
+
+const bobBuyAsset = Uniques
+  .buyItem({
+    collection: collectionId,
+    item: assetId,
+    bidPrice: price,
+  })
+  .signed({ sender: bob })
+  .sent()
+  .dbgStatus("Buying Asset:")
+  .finalized()
+
+await Rune
   .chain(() => alicePrepareCollection)
   .chain(() => bobBuyAsset)
-  .chain(() => showAssetOwner)
-
-await program.run()
-
-console.log("done")
+  .chain(() =>
+    Uniques.Asset
+      .entry(Rune.tuple([collectionId, assetId]))
+      .unhandle(undefined)
+      .access("owner")
+      .dbg("New Owner:")
+  )
+  .run()
