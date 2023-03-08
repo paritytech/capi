1eed72bac4d9f5a900c56fdab44b9f32cfd49c00
Matias Volpe <matias@parity.io>
2023-03-08 12:52:14 -0300

    feat: add custom chain spec

--- providers/frame/polkadot_dev.ts
+++ providers/frame/polkadot_dev.ts
@@ -1,3 +1,4 @@
+import { ss58, testPair } from "../../crypto/mod.ts"
 import * as $ from "../../deps/scale.ts"
 import { Env, PathInfo } from "../../server/mod.ts"
 import { getAvailable } from "../../util/port.ts"
@@ -6,6 +7,15 @@ import { FrameBinProvider } from "./FrameBinProvider.ts"
 export interface PolkadotDevProviderProps {
   polkadotPath?: string
 }
+
+const TEST_USER_COUNT = 1000
+const TEST_USER_INITIAL_FUNDS = 1000000000000000000
+const DEV_RUNTIME_PREFIXES = {
+  polkadot: 0,
+  kusama: 2,
+  westend: 42,
+  rococo: 42,
+} as const
 
 export class PolkadotDevProvider extends FrameBinProvider {
   constructor(env: Env, { polkadotPath }: PolkadotDevProviderProps = {}) {
@@ -17,13 +27,64 @@ export class PolkadotDevProvider extends FrameBinProvider {
   }
 
   async launch(pathInfo: PathInfo) {
-    const runtimeName = pathInfo.target
+    const runtimeName = pathInfo.target || "polkadot"
     $.assert($devRuntimeName, runtimeName)
     const port = getAvailable()
-    const args: string[] = ["--dev", "--ws-port", port.toString()]
-    if (runtimeName !== "polkadot") args.push(`--force-${runtimeName}`)
+    const chainSpec = await this.#getCustomChainSpec(runtimeName)
+    const args: string[] = ["--tmp", "--alice", "--ws-port", port.toString(), "--chain", chainSpec]
+    console.log({ args })
     await this.runBin(args)
     return port
+  }
+
+  async #getCustomChainSpec(
+    runtimeName: "polkadot" | "kusama" | "westend" | "rococo",
+  ): Promise<string> {
+    const buildSpecCmd = new Deno.Command(
+      this.bin,
+      {
+        args: [
+          "build-spec",
+          "--disable-default-bootnode",
+          "--chain",
+          `${runtimeName}-dev`,
+        ],
+      },
+    )
+    const chainSpec = JSON.parse(new TextDecoder().decode((await buildSpecCmd.output()).stdout))
+    const balances: any[] = chainSpec.genesis.runtime.balances.balances
+    for (let i = 0; i < TEST_USER_COUNT; i++) {
+      balances.push([
+        ss58.encode(DEV_RUNTIME_PREFIXES[runtimeName], testPair(i).publicKey),
+        TEST_USER_INITIAL_FUNDS,
+      ])
+    }
+    const customChainSpecPath = await Deno.makeTempFile({
+      prefix: `custom-${runtimeName}-chain-spec`,
+      suffix: ".json",
+    })
+    await Deno.writeTextFile(customChainSpecPath, JSON.stringify(chainSpec, undefined, 2))
+    const buildSpecRawCmd = new Deno.Command(
+      this.bin,
+      {
+        args: [
+          "build-spec",
+          "--disable-default-bootnode",
+          "--chain",
+          customChainSpecPath,
+          "--raw",
+        ],
+      },
+    )
+    const chainSpecRaw = JSON.parse(
+      new TextDecoder().decode((await buildSpecRawCmd.output()).stdout),
+    )
+    const customChainSpecRawPath = await Deno.makeTempFile({
+      prefix: `custom-${runtimeName}-chain-spec-raw`,
+      suffix: ".json",
+    })
+    await Deno.writeTextFile(customChainSpecRawPath, JSON.stringify(chainSpecRaw, undefined, 2))
+    return customChainSpecRawPath
   }
 }
 
