5f85111ab0bc911f2fb7f8e73778b0446d9bb141
Jason Butler <jasonsantiagobutler@gmail.com>
2023-03-10 01:27:55 -0300

    wip: nft pallet

--- examples/statemine.ts
+++ examples/statemine.ts
@@ -1,12 +1,27 @@
 import { $, alice, bob, MetaRune, Rune, ValueRune } from "capi"
-import { Uniques } from "zombienet/statemine.toml/collator/@latest/mod.ts"
+import { MintType } from "http://localhost:4646/frame/zombienet/zombienets/nfts.toml/collator/@v0.9.370/types/pallet_nfts/types"
+import { Nfts } from "zombienet/nfts.toml/collator/@latest/mod.ts"
 
 const collection = 1
 const item = 1
 
-const createCollection = Uniques
+// const collectionSettings = convertToByte({  })
+
+// after done minting all items, need to disable item metadata, and disable new minting (CollectionSetting.UnlockedMaxSupply), and lock collection metadata (CollectionSetting.UnlockedMetadata, .UnlockedAttributes)
+
+const createCollection = Nfts
   .create({
-    collection,
+    config: {
+      settings: BigInt(1 & 1 & 1 & 1 & 1),
+      maxSupply: undefined,
+      mintSettings: {
+        mintType: MintType.Issuer,
+        price: undefined,
+        startBlock: undefined,
+        endBlock: undefined,
+        defaultItemSettings: BigInt(1 & 1 & 1),
+      },
+    },
     admin: alice.address,
   })
   .signed({ sender: alice })
@@ -16,10 +31,9 @@ const createCollection = Uniques
 
 const $collectionMetadata = $.object($.field("hello", $.str))
 
-const setCollectionMetadata = Uniques
+const setCollectionMetadata = Nfts
   .setCollectionMetadata({
     collection,
-    isFrozen: true,
     data: $collectionMetadata.encode({ hello: "collection" }),
   })
   .signed({ sender: alice })
@@ -27,30 +41,30 @@ const setCollectionMetadata = Uniques
   .dbgStatus("Set Collection Metadata:")
   .finalized()
 
-const mintAsset = Uniques
+const mintItem = Nfts
   .mint({
     collection,
     item,
-    owner: alice.address,
+    mintTo: alice.address,
+    witnessData: undefined,
   })
   .signed({ sender: alice })
   .sent()
-  .dbgStatus("Mint Asset:")
+  .dbgStatus("Mint Item:")
   .finalized()
 
-const setAssetMetadata = Uniques
+const setItemMetadata = Nfts
   .setMetadata({
     collection,
     item,
-    isFrozen: true,
-    data: new Uint8Array(),
+    data: new Uint8Array(), // TODO: ipfs file url
   })
   .signed({ sender: alice })
   .sent()
-  .dbgStatus("Set Asset Metadata:")
+  .dbgStatus("Set Item Metadata:")
   .finalized()
 
-const setAssetPrice = Uniques
+const setItemPrice = Nfts
   .setPrice({
     collection,
     item,
@@ -59,11 +73,11 @@ const setAssetPrice = Uniques
   })
   .signed({ sender: alice })
   .sent()
-  .dbgStatus("Set Asset Price:")
+  .dbgStatus("Set Item Price:")
   .finalized()
 
 // prevent new mints in collection
-const setCollectionMaxSupply = Uniques
+const setCollectionMaxSupply = Nfts
   .setCollectionMaxSupply({
     collection,
     maxSupply: 1,
@@ -73,33 +87,66 @@ const setCollectionMaxSupply = Uniques
   .dbgStatus("Set Collection Max Supply:")
   .finalized()
 
+// const updateMintSettings = Nfts.updateMintSettings({
+//   collection,
+//   mintSettings: {
+//     mintType: MintType.Issuer,
+//     price: undefined,
+//     startBlock: undefined,
+//     endBlock: undefined,
+//     defaultItemSettings: BigInt(1 & 0 & 0), // forbid further updates of metadata
+//   }
+// })
+
+const lockItemProperties = Nfts.lockItemProperties({
+  collection,
+  item,
+  lockMetadata: true,
+  lockAttributes: true,
+})
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Lock Item Properties:")
+  .finalized()
+
+const lockCollection = Nfts.lockCollection({
+  collection,
+  lockSettings: BigInt(1 | 0 | 0 | 0 | 1), // forbit future updates of max supply
+})
+  .signed({ sender: alice })
+  .sent()
+  .dbgStatus("Lock Collection:")
+  .finalized()
+
 const alicePrepareCollection = Rune
   .chain(() => createCollection)
   .chain(() => setCollectionMetadata)
-  .chain(() => mintAsset)
-  .chain(() => setAssetMetadata)
-  .chain(() => setAssetPrice)
+  .chain(() => mintItem)
+  .chain(() => setItemMetadata)
+  .chain(() => setItemPrice)
+  .chain(() => lockItemProperties)
   .chain(() => setCollectionMaxSupply)
+  .chain(() => lockCollection)
 
-const collectionId = Uniques.Class
+const collectionId = Nfts.Collection
   .entryPage(50)
   .into(ValueRune)
   .access(0, 0, 0)
   .unhandle(undefined)
 
-const assetId = collectionId.map((collectionId) => Uniques.Asset.entryPage(50, [collectionId]))
+const assetId = collectionId.map((collectionId) => Nfts.Item.entryPage(50, [collectionId]))
   .into(MetaRune)
   .flat()
   .into(ValueRune)
   .access(0, 0, 1)
   .unhandle(undefined)
 
-const price = Uniques.ItemPriceOf
+const price = Nfts.ItemPriceOf
   .entry(Rune.tuple([collectionId, assetId]))
   .unhandle(undefined)
   .access(0)
 
-const bobBuyAsset = Uniques
+const bobBuyItem = Nfts
   .buyItem({
     collection: collectionId,
     item: assetId,
@@ -107,14 +154,14 @@ const bobBuyAsset = Uniques
   })
   .signed({ sender: bob })
   .sent()
-  .dbgStatus("Buying Asset:")
+  .dbgStatus("Buying Item:")
   .finalized()
 
 await Rune
   .chain(() => alicePrepareCollection)
-  .chain(() => bobBuyAsset)
+  .chain(() => bobBuyItem)
   .chain(() =>
-    Uniques.Asset
+    Nfts.Item
       .entry(Rune.tuple([collectionId, assetId]))
       .unhandle(undefined)
       .access("owner")
