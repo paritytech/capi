1dd57877aa35e93f3991cfd2419a39655d19f0ca
Ryan Lee <ryan@parity.io>
2023-03-10 14:24:42 -0500

    a

--- .github/actions/troodontid/entrypoint.sh
+++ .github/actions/troodontid/entrypoint.sh
@@ -4,5 +4,5 @@ echo "test args $1 $2 $3 $4 $5"
 
 echo $GITHUB_STEP_SUMMARY
 mkdir -p "$(dirname $GITHUB_STEP_SUMMARY)" && touch "$GITHUB_STEP_SUMMARY"
-RUN ls -la $GITHUB_STEP_SUMMARY
+ls -la $GITHUB_STEP_SUMMARY
 deno task capi -- deno run -A -r=http://localhost:4646/ .github/actions/troodontid/main.ts --dir $1 --importMap $2
