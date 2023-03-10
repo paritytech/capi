6b9b57f301a5330d9df55ee3d151f1199c3305b1
tjjfvi <t6@t6.fyi>
2023-03-09 19:23:26 -0500

    override ChainError.stack

--- scale_info/overrides/ChainError.ts
+++ scale_info/overrides/ChainError.ts
@@ -2,6 +2,7 @@ export class ChainError<T> extends Error {
   override readonly name = "ChainError"
   constructor(public value: T) {
     super()
+    this.stack = "ChainError: see error.value\n  [error occurred on chain]"
   }
 
   static toArgs = <T>(x: ChainError<T>): [T] => [x.value]
