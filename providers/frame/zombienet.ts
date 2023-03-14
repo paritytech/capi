2668a2a16d480449084fbaa8ce28bb38c3de5427
tjjfvi <t6@t6.fyi>
2023-03-14 09:02:22 -0400

    remove all references to zombienet bin

--- providers/frame/zombienet.ts
+++ providers/frame/zombienet.ts
@@ -2,29 +2,21 @@ import { readLines } from "../../deps/std/io.ts"
 import { readerFromStreamReader } from "../../deps/std/streams.ts"
 import { unreachable } from "../../deps/std/testing/asserts.ts"
 import { Network, readNetworkConfig } from "../../deps/zombienet.ts"
-import { Env, PathInfo } from "../../server/mod.ts"
+import { PathInfo } from "../../server/mod.ts"
 import { PermanentMemo } from "../../util/mod.ts"
-import { FrameBinProvider } from "./FrameBinProvider.ts"
+import { FrameProxyProvider } from "./FrameProxyProvider.ts"
 
 export interface ZombienetProviderProps {
   zombienetPath?: string
 }
 
-export class ZombienetProvider extends FrameBinProvider {
-  constructor(env: Env, { zombienetPath }: ZombienetProviderProps = {}) {
-    super(env, {
-      bin: zombienetPath ?? ZOMBIENET_BIN_DEFAULTS[Deno.build.os]!,
-      installation: "https://github.com/paritytech/zombienet",
-      readyTimeout: 3 * 60 * 1000,
-    })
-  }
-
-  launchMemo = new PermanentMemo<string, Record<string, string>>()
-  async launch(pathInfo: PathInfo) {
+export class ZombienetProvider extends FrameProxyProvider {
+  networkMemo = new PermanentMemo<string, Record<string, string>>()
+  async dynamicUrl(pathInfo: PathInfo) {
     const target = pathInfo.target!
     const i = target.lastIndexOf("/")
     const configPath = target.slice(0, i)
-    const network = await this.launchMemo.run(configPath, async () => {
+    const network = await this.networkMemo.run(configPath, async () => {
       const zombiecache = await Deno.realPath(await Deno.makeTempDir({ prefix: `capi_zombienet_` }))
       const config = readNetworkConfig(configPath)
       ;(config.settings ??= { provider: "native", timeout: 1200 }).provider = "native"
@@ -56,9 +48,9 @@ export class ZombienetProvider extends FrameBinProvider {
       unreachable()
     })
     const nodeName = target.slice(i + 1)
-    const node = network[nodeName]
-    if (!node) throw new Error()
-    return +new URL(node).port
+    const url = network[nodeName]
+    if (!url) throw new Error()
+    return url
   }
 
   async network(zombienetCachePath: string, networkManifestPath: string): Promise<Network> {
@@ -70,14 +62,4 @@ export class ZombienetProvider extends FrameBinProvider {
     }
     return unreachable()
   }
-}
-
-const ZOMBIENET_BIN_DEFAULTS: Record<string, string> = {
-  darwin: "zombienet-macos",
-  linux: "zombienet-linux-x64",
-}
-
-interface ZombienetLaunchInfo {
-  configPath: string
-  nodeName: string
 }
