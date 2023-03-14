e9d3f93061e783d3275fdc2d2ebdff58abd964a6
Ryan Lee <ryan@parity.io>
2023-03-14 16:21:45 -0400

    chore: wrap error unhandling example in assertRejects

--- examples/error_unhandling.ts
+++ examples/error_unhandling.ts
@@ -1,14 +1,16 @@
 import { alice, bob } from "capi"
 import { Balances } from "westend_dev/mod.ts"
+import { assertRejects } from "../deps/std/testing/asserts.ts"
 
-await Balances
-  .transfer({
-    value: 1_000_000_000_000_000_000_000_000_000_000_000_000n,
-    dest: bob.address,
-  })
-  .signed({ sender: alice })
-  .sent()
-  .dbgStatus()
-  .finalizedEvents()
-  .unhandleFailed()
-  .run()
+assertRejects(() =>
+  Balances
+    .transfer({
+      value: 1_000_000_000_000_000_000_000_000_000_000_000_000n,
+      dest: bob.address,
+    })
+    .signed({ sender: alice })
+    .sent()
+    .dbgStatus()
+    .finalizedEvents()
+    .unhandleFailed()
+    .run(), "unhandleFailed")
