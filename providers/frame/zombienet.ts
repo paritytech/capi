f56f6b05f8326a32764e6eb57ba46c4fce6f2e40
tjjfvi <t6@t6.fyi>
2023-03-15 10:42:36 -0400

    use zombienet programmatic api directly

--- providers/frame/zombienet.ts
+++ providers/frame/zombienet.ts
@@ -1,7 +1,5 @@
-import { readLines } from "../../deps/std/io.ts"
-import { readerFromStreamReader } from "../../deps/std/streams.ts"
 import { unreachable } from "../../deps/std/testing/asserts.ts"
-import { Network, readNetworkConfig } from "../../deps/zombienet.ts"
+import { Network, readNetworkConfig, start } from "../../deps/zombienet.ts"
 import { PathInfo } from "../../server/mod.ts"
 import { PermanentMemo } from "../../util/mod.ts"
 import { FrameProxyProvider } from "./FrameProxyProvider.ts"
@@ -11,7 +9,7 @@ export interface ZombienetProviderProps {
 }
 
 export class ZombienetProvider extends FrameProxyProvider {
-  networkMemo = new PermanentMemo<string, Record<string, string>>()
+  networkMemo = new PermanentMemo<string, Network>()
   async dynamicUrl(pathInfo: PathInfo) {
     const target = pathInfo.target!
     const i = target.lastIndexOf("/")
@@ -27,28 +25,14 @@ export class ZombienetProvider extends FrameProxyProvider {
         force: true,
         inCI: false,
       }
-      const child = new Deno.Command(Deno.execPath(), {
-        args: [
-          "run",
-          "-A",
-          import.meta.resolve("./zombienet_worker.ts"),
-          JSON.stringify([config, options]),
-        ],
-        stdout: "piped",
-        stderr: "piped",
-      }).spawn()
+      const network = await start("", config, options)
       this.env.signal.addEventListener("abort", () => {
-        child.kill("SIGINT")
+        network.stop()
       })
-      for await (const line of readLines(readerFromStreamReader(child.stdout.getReader()))) {
-        if (!line.startsWith("capi_network = ")) continue
-        const network = JSON.parse(line.slice("capi_network = ".length)) as Record<string, string>
-        return network
-      }
-      unreachable()
+      return network
     })
     const nodeName = target.slice(i + 1)
-    const url = network[nodeName]
+    const url = network.nodesByName[nodeName]?.wsUri
     if (!url) throw new Error()
     return url
   }
