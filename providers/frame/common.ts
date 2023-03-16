f274aed8577505622168639fa2579f60b14507fb
Matias Volpe <matias@parity.io>
2023-03-16 12:53:47 -0300

    feat: rename _tasks/download.ts to _tasks/generate_artifacts.ts

--- providers/frame/common.ts
+++ providers/frame/common.ts
@@ -1,8 +1,9 @@
 import { ss58 } from "../../crypto/mod.ts"
 import * as $ from "../../deps/scale.ts"
-import { DEFAULT_TEST_USER_COUNT } from "../../util/generateTestUserPublicKeys.ts"
 
+export const DEFAULT_TEST_USER_COUNT = 100_000
 const DEFAULT_TEST_USER_INITIAL_FUNDS = 1_000_000_000_000_000_000
+export const publicKeysPath = new URL("./test_users_public_keys.scale", import.meta.url)
 
 export async function createCustomChainSpec(
   bin: string,
@@ -14,9 +15,7 @@ export async function createCustomChainSpec(
   })
   const chainSpec = JSON.parse(new TextDecoder().decode((await buildSpecCmd.output()).stdout))
   const balances: [string, number][] = chainSpec.genesis.runtime.balances.balances
-  const publicKeys = $.array($.sizedUint8Array(32)).decode(
-    await Deno.readFile(new URL(`./test_users_public_keys.scale`, import.meta.url)),
-  )
+  const publicKeys = $.array($.sizedUint8Array(32)).decode(await Deno.readFile(publicKeysPath))
   for (let i = 0; i < DEFAULT_TEST_USER_COUNT; i++) {
     balances.push([ss58.encode(networkPrefix, publicKeys[i]!), DEFAULT_TEST_USER_INITIAL_FUNDS])
   }
--- util/mod.ts
+++ util/mod.ts
@@ -1,4 +1,4 @@
-// moderate --exclude cache port.ts tsFormatter.ts generateTestUserPublicKeys.ts
+// moderate --exclude cache port.ts tsFormatter.ts
 
 export * from "./ArrayOfLength.ts"
 export * from "./clock.ts"
