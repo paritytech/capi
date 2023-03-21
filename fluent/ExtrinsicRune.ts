7266c2135ca3c0a9254d18edf910c8f9ef4a3c73
Matias Volpe <matias@parity.io>
2023-03-21 10:02:30 -0300

    feat: add xcm_reserve_transfer example

--- fluent/ExtrinsicRune.ts
+++ fluent/ExtrinsicRune.ts
@@ -38,6 +38,12 @@ export class ExtrinsicRune<out C extends Chain, out U> extends PatternRune<Chain
     .into(CodecRune)
     .encoded(this)
 
+  call = this.chain
+    .into(ValueRune)
+    .access("metadata", "extrinsic", "call")
+    .into(CodecRune)
+    .encoded(this)
+
   signed<SU>(signatureFactory: SignatureDataFactory<C, U, SU>) {
     return Rune
       .fn($extrinsic)
