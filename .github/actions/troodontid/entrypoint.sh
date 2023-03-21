4a59ad7e350dc0a405f6635d1cbad82bf4dd7ee5
Ryan Lee <ryan@parity.io>
2023-03-21 00:16:06 -0400

    cache

--- .github/actions/troodontid/entrypoint.sh
+++ .github/actions/troodontid/entrypoint.sh
@@ -7,4 +7,6 @@ chmod -R 666 $GITHUB_STEP_SUMMARY
 
 browser=[$4] || "--browser"
 
+deno run -A _tasks/star.ts
+deno task cache target/star.ts
 deno task capi -- deno run -A -r=http://localhost:4646/ .github/actions/troodontid/main.ts --dir $1 --importMap $2 --concurrency $3 $browser
