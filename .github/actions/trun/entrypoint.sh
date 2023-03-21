3e2c85c552032c32e5cc5139ca90de86f6ec0e96
Ryan Lee <ryan@parity.io>
2023-03-21 13:10:58 -0400

    update entrypoint script

--- .github/actions/trun/entrypoint.sh
+++ .github/actions/trun/entrypoint.sh
@@ -7,7 +7,7 @@ chmod -R 666 $GITHUB_STEP_SUMMARY
 
 browser=[$4] || "--browser"
 
-deno run -A validate_ignore.ts --dir examples
+deno run -A /trun/validate_ignore.ts --dir examples
 deno run -A _tasks/star.ts
 deno task cache target/star.ts
-deno task capi -- deno run -A -r=http://localhost:4646/ .github/actions/trun/main.ts --dir $1 --import-map $2 --concurrency $3 $browser
+deno task capi -- deno run -A -r=http://localhost:4646/ /trun/main.ts --dir $1 --import-map $2 --concurrency $3 $browser
