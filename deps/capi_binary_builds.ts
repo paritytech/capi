9e674b5a131972b9d3713c89039536a82f0243c0
tjjfvi <t6@t6.fyi>
2023-03-22 10:27:02 -0400

    bump cbb

--- deps/capi_binary_builds.ts
+++ deps/capi_binary_builds.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/paritytech/capi-binary-builds/4d237dd/download.ts"
+export * from "https://raw.githubusercontent.com/paritytech/capi-binary-builds/ee456af/download.ts"
