c3e7a99d9ff78e1cbb8167a92b940749b1a4a1d1
tjjfvi <t6@t6.fyi>
2023-03-22 12:46:18 -0400

    downgrade cumulus & uncomment xcm example

--- examples/xcm_asset_teleportation.ts
+++ examples/xcm_asset_teleportation.ts
@@ -1,88 +1,86 @@
-// TODO: update for xcm v2/v3
+import { alice, Rune, ValueRune } from "capi"
+import { types, XcmPallet } from "zombienet/statemine.toml/alice/@latest/mod.js"
+import { Event as XcmPalletEvent } from "zombienet/statemine.toml/alice/@latest/types/pallet_xcm/pallet.js"
+import { RuntimeEvent as AliceRuntimeEvent } from "zombienet/statemine.toml/alice/@latest/types/rococo_runtime/mod.js"
+import { chain as collator, System } from "zombienet/statemine.toml/collator/@latest/mod.js"
+import { Event as ParachainSystemEvent } from "zombienet/statemine.toml/collator/@latest/types/cumulus_pallet_parachain_system/pallet.js"
+import { RuntimeEvent as CollatorRuntimeEvent } from "zombienet/statemine.toml/collator/@latest/types/statemine_runtime.js"
+import { signature } from "../patterns/signature/polkadot.ts"
 
-// import { alice, Rune, ValueRune } from "capi"
-// import { types, XcmPallet } from "zombienet/statemine.toml/alice/@latest/mod.js"
-// import { Event as XcmPalletEvent } from "zombienet/statemine.toml/alice/@latest/types/pallet_xcm/pallet.js"
-// import { RuntimeEvent as AliceRuntimeEvent } from "zombienet/statemine.toml/alice/@latest/types/rococo_runtime/mod.js"
-// import { chain as collator, System } from "zombienet/statemine.toml/collator/@latest/mod.js"
-// import { Event as ParachainSystemEvent } from "zombienet/statemine.toml/collator/@latest/types/cumulus_pallet_parachain_system/pallet.js"
-// import { RuntimeEvent as CollatorRuntimeEvent } from "zombienet/statemine.toml/collator/@latest/types/statemine_runtime.js"
-// import { signature } from "../patterns/signature/polkadot.ts"
+const {
+  VersionedMultiAssets,
+  VersionedMultiLocation,
+  v1: {
+    junction: { Junction },
+    multilocation: { Junctions },
+    multiasset: { AssetId, Fungibility },
+  },
+  v0: { junction: { NetworkId } },
+  v2: { WeightLimit },
+} = types.xcm
 
-// const {
-//   VersionedMultiAssets,
-//   VersionedMultiLocation,
-//   v1: {
-//     junction: { Junction },
-//     multilocation: { Junctions },
-//     multiasset: { AssetId, Fungibility },
-//   },
-//   v0: { junction: { NetworkId } },
-//   v2: { WeightLimit },
-// } = types.xcm
+class CannotFindAttemptError extends Error {
+  override readonly name = "CannotFindAttemptError"
+}
 
-// class CannotFindAttemptError extends Error {
-//   override readonly name = "CannotFindAttemptError"
-// }
+const initiatedEvent = XcmPallet
+  .limitedTeleportAssets({
+    dest: VersionedMultiLocation.V1(Rune.rec({
+      parents: 0,
+      interior: Junctions.X1(Junction.Parachain(1000)),
+    })),
+    beneficiary: VersionedMultiLocation.V1(Rune.rec({
+      parents: 0,
+      interior: Junctions.X1(Junction.AccountId32({
+        id: alice.publicKey,
+        network: NetworkId.Any(),
+      })),
+    })),
+    assets: VersionedMultiAssets.V1(Rune.array([Rune.rec({
+      id: AssetId.Concrete(Rune.rec({
+        parents: 0,
+        interior: Junctions.Here(),
+      })),
+      fun: Fungibility.Fungible(500_000_000_000_000n),
+    })])),
+    feeAssetItem: 0,
+    weightLimit: WeightLimit.Unlimited(),
+  })
+  .signed(signature({ sender: alice }))
+  .sent()
+  .dbgStatus("Teleportation status:")
+  .finalizedEvents()
+  .into(ValueRune)
+  .map((events) =>
+    events.find((e) =>
+      AliceRuntimeEvent.isXcmPallet(e.event)
+      && XcmPalletEvent.isAttempted(e.event.value)
+    ) ?? new CannotFindAttemptError()
+  )
+  .unhandle(CannotFindAttemptError)
+  .dbg("Initiated event:")
 
-// const initiatedEvent = XcmPallet
-//   .limitedTeleportAssets({
-//     dest: VersionedMultiLocation.V1(Rune.rec({
-//       parents: 0,
-//       interior: Junctions.X1(Junction.Parachain(1000)),
-//     })),
-//     beneficiary: VersionedMultiLocation.V1(Rune.rec({
-//       parents: 0,
-//       interior: Junctions.X1(Junction.AccountId32({
-//         id: alice.publicKey,
-//         network: NetworkId.Any(),
-//       })),
-//     })),
-//     assets: VersionedMultiAssets.V1(Rune.array([Rune.rec({
-//       id: AssetId.Concrete(Rune.rec({
-//         parents: 0,
-//         interior: Junctions.Here(),
-//       })),
-//       fun: Fungibility.Fungible(500_000_000_000_000n),
-//     })])),
-//     feeAssetItem: 0,
-//     weightLimit: WeightLimit.Unlimited(),
-//   })
-//   .signed(signature({ sender: alice }))
-//   .sent()
-//   .dbgStatus("Teleportation status:")
-//   .finalizedEvents()
-//   .into(ValueRune)
-//   .map((events) =>
-//     events.find((e) =>
-//       AliceRuntimeEvent.isXcmPallet(e.event)
-//       && XcmPalletEvent.isAttempted(e.event.value)
-//     ) ?? new CannotFindAttemptError()
-//   )
-//   .unhandle(CannotFindAttemptError)
-//   .dbg("Initiated event:")
+const processedEvent = System.Events
+  .value(undefined, collator.latestBlock.hash)
+  .map((events) =>
+    events?.find((e) =>
+      CollatorRuntimeEvent.isParachainSystem(e.event)
+      && ParachainSystemEvent.isDownwardMessagesProcessed(e.event.value)
+    )
+  )
+  .filter((event) => !!event)
+  .dbg("Processed events:")
 
-// const processedEvent = System.Events
-//   .value(undefined, collator.latestBlock.hash)
-//   .map((events) =>
-//     events?.find((e) =>
-//       CollatorRuntimeEvent.isParachainSystem(e.event)
-//       && ParachainSystemEvent.isDownwardMessagesProcessed(e.event.value)
-//     )
-//   )
-//   .filter((event) => !!event)
-//   .dbg("Processed events:")
+// TODO: have the recipient associate the downward message with the sender
+await logAliceBalance()
+  .chain(() => Rune.tuple([initiatedEvent, processedEvent]))
+  .chain(() => logAliceBalance())
+  .run()
 
-// // TODO: have the recipient associate the downward message with the sender
-// await logAliceBalance()
-//   .chain(() => Rune.tuple([initiatedEvent, processedEvent]))
-//   .chain(() => logAliceBalance())
-//   .run()
-
-// function logAliceBalance() {
-//   return System.Account
-//     .value(alice.publicKey)
-//     .unhandle(undefined)
-//     .access("data", "free")
-//     .dbg("Alice balance:")
-// }
+function logAliceBalance() {
+  return System.Account
+    .value(alice.publicKey)
+    .unhandle(undefined)
+    .access("data", "free")
+    .dbg("Alice balance:")
+}
