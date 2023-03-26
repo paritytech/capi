740a528bd39a38295d84fad7fd848661b083c18f
Harry Solovay <harrysolovay@gmail.com>
2023-03-25 20:08:45 -0400

    continued

--- fluent/constraints/ConstantChain.ts
+++ fluent/constraints/ConstantChain.ts
@@ -1,8 +1,14 @@
-import * as $ from "../../deps/scale.ts"
 import { FrameMetadata } from "../../frame_metadata/FrameMetadata.ts"
 import { Chain } from "../ChainRune.ts"
 
-export interface ConstantConstraint<
+export type ConstantChain<
+  C extends Chain,
+  P extends string,
+  K extends string,
+  V extends Partial<FrameMetadata.Constant>,
+> = ConstantConstraint<P, K, V> extends C ? never : C
+
+interface ConstantConstraint<
   P extends string = string,
   N extends string = string,
   C extends Partial<FrameMetadata.Constant> = Partial<FrameMetadata.Constant>,
@@ -13,10 +19,3 @@ export interface ConstantConstraint<
     }>
   }
 }
-
-export type ConstantChain<
-  C extends Chain,
-  P extends string,
-  K extends string,
-  V extends Partial<FrameMetadata.Constant>,
-> = ConstantConstraint<P, K, V> extends C ? never : C
--- fluent/constraints/StorageChain.ts
+++ fluent/constraints/StorageChain.ts
@@ -1,7 +1,14 @@
 import { FrameMetadata } from "../../frame_metadata/FrameMetadata.ts"
 import { Chain } from "../ChainRune.ts"
 
-export interface StorageConstraint<
+export type StorageChain<
+  C extends Chain,
+  P extends string,
+  K extends string,
+  V extends Partial<FrameMetadata.StorageEntry>,
+> = C extends StorageConstraint<P, K, V> ? C : never
+
+interface StorageConstraint<
   P extends string = string,
   N extends string = string,
   S extends Partial<FrameMetadata.StorageEntry> = Partial<FrameMetadata.StorageEntry>,
@@ -12,10 +19,3 @@ export interface StorageConstraint<
     }>
   }
 }
-
-export type StorageChain<
-  C extends Chain,
-  P extends string,
-  K extends string,
-  V extends Partial<FrameMetadata.StorageEntry>,
-> = C extends StorageConstraint<P, K, V> ? C : never
