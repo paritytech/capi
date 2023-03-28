95ff533e12088c084b0927204d8630c73b493335
Harry Solovay <harrysolovay@gmail.com>
2023-03-28 15:34:31 -0400

    beginnings

---
+++ examples/test_users.ts
@@ -0,0 +1,18 @@
+/**
+ * @title Create Test Users
+ *
+ * When developing against a test chain, the `createUsers` utility
+ * provides `Sr25519` instances, which correspond to test users (pre-seeded with funds).
+ * This simplifies signing extrinsics for submission to the given test chain.
+ */
+
+import { createUsers } from "polkadot_dev/mod.js"
+
+// Test users can be initialized with no count. The resulting collection is
+// a record with 26 `Sr25519`s (one for every letter of the alphabet).
+// deno-lint-ignore no-unused-vars
+const { alexa, billy, carol } = await createUsers()
+
+// If count is specified, the result is an `Sr25519` tuple of the specified length.
+// deno-lint-ignore no-unused-vars
+const [david, ellie, felix] = await createUsers(3)
