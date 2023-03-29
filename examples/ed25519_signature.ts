f38c09e1d5ba75eb2e7ecc0810badefed1ef7959
Ryan Lee <ryan@parity.io>
2023-03-29 17:02:03 -0400

    use multiaddress constructor

--- examples/ed25519_signature.ts
+++ examples/ed25519_signature.ts
@@ -1,5 +1,7 @@
 import { Balances, users } from "westend_dev/mod.js"
+import { MultiAddress } from "westend_dev/types/sp_runtime/multiaddress.js"
 import * as ed from "../deps/ed25519.ts"
+import { Rune } from "../mod.ts"
 import { signature } from "../patterns/signature/polkadot.ts"
 
 const [alexa, billy, eddie] = await users(3)
@@ -7,22 +9,19 @@ const [alexa, billy, eddie] = await users(3)
 const ed25519PrivateKey = eddie.secretKey.slice(0, 32)
 const ed25519PublicKey = await ed.getPublicKey(ed25519PrivateKey)
 
-const ed25519Eddie = {
-  address: {
-    type: "Id" as const,
-    value: ed25519PublicKey,
-  },
+const ed25519Eddie = Rune.rec({
+  address: MultiAddress.Id(ed25519PublicKey),
   sign: (msg: Uint8Array) => ({
     type: "Ed25519" as const,
     value: ed.sync.sign(msg, ed25519PrivateKey),
   }),
-}
+})
 
 // Existential Deposit
 await Balances
   .transfer({
     value: 1_000_000_000_000n,
-    dest: ed25519Eddie.address,
+    dest: ed25519Eddie.access("address"),
   })
   .signed(signature({ sender: alexa }))
   .sent()
