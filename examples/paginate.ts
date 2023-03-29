705340f02ca266328fb181f8ad6df69d5f91b90d
Harry Solovay <harrysolovay@gmail.com>
2023-03-29 07:51:49 -0400

    continued

--- examples/paginate.ts
+++ examples/paginate.ts
@@ -1,24 +1,21 @@
-import { $, Rune } from "capi"
+import { $ } from "capi"
 import { System } from "polkadot_dev/mod.js"
 import { types, Uniques } from "statemint/mod.js"
 
 // Reference the first 10 keys of a polkadot dev chain's system account map.
-const accountKeys = System.Account.keyPage(10, null)
+const accountKeys = await System.Account.keyPage(10, null).run()
+
+// Each key should be of type `Uint8Array`.
+$.is($.sizedArray($.uint8Array, 10), accountKeys)
 
 // Reference the first 10 key-value pairs of a statemint dev chain's uniques class map.
 const collectionsEntries = Uniques.Class.entryPage(10, null)
 
-// Evaluate both in parallel and return the results in a record.
-const result = await Rune.rec({ accountKeys, collectionsEntries }).run()
-
-// Each key should be of type `Uint8Array`.
-$.is($tupleOf($.uint8Array, 10), result.accountKeys)
-
 // Each entry should be of type `[number, types.pallet_uniques.types.CollectionDetails]`
-const $collectionEntry = $.tuple($.u8, types.pallet_uniques.types.$collectionDetails)
-$.is($tupleOf($collectionEntry, 10), result.collectionsEntries)
-
-// This helper function simplifies creating codecs for page assertion.
-function $tupleOf<N>(value: $.Codec<N>, length: number) {
-  return $.tuple(...Array.from({ length }, () => value))
-}
+$.is(
+  $.sizedArray(
+    $.tuple($.u8, types.pallet_uniques.types.$collectionDetails),
+    10,
+  ),
+  collectionsEntries,
+)
