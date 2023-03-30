15f46f3101f4f03c50cb08065480d1cc15f0227c
Harry Solovay <harrysolovay@gmail.com>
2023-03-30 10:27:22 -0400

    continued

--- deps/scale.ts
+++ deps/scale.ts
@@ -1,11 +1,1 @@
-import { array, Codec, transform, tuple } from "https://deno.land/x/scale@v0.11.0/mod.ts#="
-
-export * from "https://deno.land/x/scale@v0.11.0/mod.ts#="
-
-export function record<K extends keyof any, V>(key: Codec<K>, value: Codec<V>) {
-  return transform<[K, V][], Record<K, V>>({
-    $base: array(tuple(key, value)),
-    encode: Object.entries as (value: Record<K, V>) => [K, V][],
-    decode: Object.fromEntries,
-  })
-}
+export * from "https://deno.land/x/scale@v0.11.1/mod.ts#="
