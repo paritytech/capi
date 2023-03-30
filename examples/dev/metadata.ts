0bcae1fa3ec2c0a13c299e64c3f2d88e7ed5024b
Harry Solovay <harrysolovay@gmail.com>
2023-03-30 11:54:42 -0400

    cleanup

--- examples/dev/metadata.ts
+++ examples/dev/metadata.ts
@@ -6,7 +6,7 @@
  *
  * The chain's metadata (formally-termed "FRAME Metadata") describes all of its runtime properties,
  * including storage, constants, callables, their types and even plain-text descriptions.
- * This metadata serves as the basis off of which we generate Chain-specific APIs. Unless you are
+ * This metadata serves as the basis off of which we generate chain-specific APIs. Unless you are
  * building an advanced Capi-based library, chances are that you don't need to work with the metadata directly.
  */
 
