cf1b7ca4680837bf5df9277d984ffefccc115d39
Harry Solovay <harrysolovay@gmail.com>
2023-03-31 09:48:42 -0400

    new scale version

--- deps/scale.ts
+++ deps/scale.ts
@@ -1,2 +1,1 @@
-// TODO: swap out with deno.land/x specifier once merge of https://github.com/paritytech/scale-ts/pull/148#event-8897955196
-export * from "https://raw.githubusercontent.com/paritytech/scale-ts/nullable/mod.ts"
+export * from "https://deno.land/x/scale@v0.11.2/mod.ts#="
