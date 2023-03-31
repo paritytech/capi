81919c42bb84197f06b6b20e2f1d917aa0dcd6b6
Harry Solovay <harrysolovay@gmail.com>
2023-03-31 09:17:48 -0400

    starignore ink

--- _tasks/star.ts
+++ _tasks/star.ts
@@ -5,7 +5,7 @@ let generated = ""
 for await (
   const entry of fs.walk(".", {
     match: [/\.ts$/],
-    skip: [/^target\//],
+    skip: [/^target\//, path.globToRegExp("examples/ink/*.ts")],
   })
 ) {
   generated += `import ${JSON.stringify(`../${entry.path}`)};\n`
