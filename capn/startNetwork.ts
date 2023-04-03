ee2469d6b7cbe4ae1d65d3626f40d3e95665fe60
tjjfvi <t6@t6.fyi>
2023-04-03 09:07:49 -0400

    abstract node spawning

--- capn/startNetwork.ts
+++ capn/startNetwork.ts
@@ -1,5 +1,6 @@
 import { download } from "../deps/capi_binary_builds.ts"
 import * as path from "../deps/std/path.ts"
+import { writableStreamFromWriter } from "../deps/std/streams.ts"
 import { getFreePort } from "../util/port.ts"
 
 if (import.meta.main) {
@@ -33,21 +34,13 @@ async function startNetwork(signal: AbortSignal) {
     },
   )
 
-  const parachainWasm = await parachainExportGenesis(
+  const parachainGenesis = await exportParachainGenesis(
     parachainCmd,
     parachainRawChainSpecPath,
-    "wasm",
-    signal,
-  )
-  const parachainState = await parachainExportGenesis(
-    parachainCmd,
-    parachainRawChainSpecPath,
-    "state",
     signal,
   )
 
   const relayCmd = await download("polkadot", "v0.9.37")
-  const nodeKey = await generateNodeKey(relayCmd, signal)
 
   const relayRawChainSpecPath = await createCustomChainSpec(
     tempDir,
@@ -57,309 +50,59 @@ async function startNetwork(signal: AbortSignal) {
     (chainSpec: ChainSpec) => {
       // TODO: add custom account balances
       chainSpec.genesis.runtime.runtime_genesis_config.paras.paras.push(
-        [parachainId, [parachainState, parachainWasm, true]],
+        [parachainId, [...parachainGenesis, true]],
       )
       return chainSpec
     },
   )
 
-  const relayBootNodePath = path.join(tempDir, "relay-bootnode")
-  const relayBootNode = spawnRelayBootNode({
-    command: relayCmd,
-    chain: relayRawChainSpecPath,
-    keystoreAccount: "alice",
-    basePath: relayBootNodePath,
-    nodeKey: nodeKey.nodeKey,
+  const relayBootnodes = await spawnNodes(
+    path.join(tempDir, "relay"),
+    relayCmd,
+    relayRawChainSpecPath,
+    8,
+    [],
     signal,
-  })
-  console.log({ relayBootNode })
+  )
 
-  const relayNodePath = path.join(tempDir, "relay-node")
-  const relayBootNodes = generateBootnodeString(relayBootNode.port, nodeKey.peerId)
-  const relayNode = spawnRelayNode({
-    command: relayCmd,
-    chain: relayRawChainSpecPath,
-    keystoreAccount: "bob",
-    basePath: relayNodePath,
-    bootnodes: relayBootNodes,
+  await spawnNodes(
+    path.join(tempDir, "parachain"),
+    parachainCmd,
+    parachainRawChainSpecPath,
+    8,
+    [
+      "--",
+      "--execution",
+      "wasm",
+      "--chain",
+      relayRawChainSpecPath,
+      "--bootnodes",
+      relayBootnodes,
+    ],
     signal,
-  })
-  console.log({ relayNode })
-
-  const parachainNodeKey = await generateNodeKey(parachainCmd, signal)
-  const parachainBootNodePath = path.join(tempDir, "parachain-bootnode")
-  const parachainBootNode = spawnParachainBootNode({
-    command: parachainCmd,
-    chain: parachainRawChainSpecPath,
-    keystoreAccount: "alice",
-    basePath: parachainBootNodePath,
-    nodeKey: parachainNodeKey.nodeKey,
-    relayChain: relayRawChainSpecPath,
-    relayBootnodes: relayBootNodes,
-    signal,
-  })
-  console.log({ parachainBootNode })
-
-  const parachainBootNodes = generateBootnodeString(parachainBootNode.port, parachainNodeKey.peerId)
-  const parachainNodePath = path.join(tempDir, "parachain-node")
-  const parachainNode = spawnParachainNode({
-    command: parachainCmd,
-    chain: parachainRawChainSpecPath,
-    keystoreAccount: "bob",
-    basePath: parachainNodePath,
-    bootnodes: parachainBootNodes,
-    relayChain: relayRawChainSpecPath,
-    relayBootnodes: relayBootNodes,
-    signal,
-  })
-  console.log({ parachainNode })
-
-  // signal.addEventListener("abort", () => {
-  //   relayBootNode.child.kill("SIGKILL")
-  //   relayNode.child.kill("SIGKILL")
-  //   parachainBootNode.child.kill("SIGKILL")
-  //   parachainNode.child.kill("SIGKILL")
-  // })
+  )
 }
 
-async function parachainExportGenesis(
+async function exportParachainGenesis(
   binary: string,
   chain: string,
-  type: "wasm" | "state",
   signal: AbortSignal,
 ) {
-  const { success, stdout } = await new Deno.Command(binary, {
-    args: [`export-genesis-${type}`, "--chain", chain],
-    signal,
-  }).output()
-  if (!success) {
-    throw new Error()
-  }
-  return new TextDecoder().decode(stdout)
-}
-
-interface SpawnParachainNodeProps {
-  command: string
-  chain: string
-  keystoreAccount: string
-  basePath: string
-  bootnodes: string
-  relayChain: string
-  relayBootnodes: string
-  signal: AbortSignal
-}
-
-function spawnParachainNode({
-  command,
-  chain,
-  keystoreAccount,
-  basePath,
-  bootnodes,
-  relayChain,
-  relayBootnodes,
-  signal,
-}: SpawnParachainNodeProps) {
-  const [port, wsPort] = [getFreePort(), getFreePort()]
-  const child = new Deno.Command(command, {
-    args: [
-      "--collator",
-      `--${keystoreAccount}`,
-      "--force-authoring",
-      "--base-path",
-      basePath,
-      "--chain",
-      chain,
-      "--port",
-      `${port}`,
-      "--ws-port",
-      `${wsPort}`,
-      "--bootnodes",
-      bootnodes,
-      "--",
-      "--execution",
-      "wasm",
-      "--chain",
-      relayChain,
-      "--bootnodes",
-      relayBootnodes,
-    ],
-    stdout: "inherit",
-    stderr: "inherit",
-    signal,
-  }).spawn()
-
-  // TODO: redirect stdout/stderr to a file
-
-  // TODO: handle child early exit
-  // child.status.then(({ success }) => {
-  //   if (!success) {
-  //   }
-  // })
-
-  return { child, port, wsPort }
-}
-
-interface SpawnParachainBootNodeProps {
-  command: string
-  chain: string
-  keystoreAccount: string
-  basePath: string
-  nodeKey: string
-  relayChain: string
-  relayBootnodes: string
-  signal: AbortSignal
-}
-
-function spawnParachainBootNode({
-  command,
-  chain,
-  keystoreAccount,
-  basePath,
-  nodeKey,
-  relayChain,
-  relayBootnodes,
-  signal,
-}: SpawnParachainBootNodeProps) {
-  const [port, wsPort] = [getFreePort(), getFreePort()]
-  const child = new Deno.Command(command, {
-    args: [
-      "--collator",
-      `--${keystoreAccount}`,
-      "--force-authoring",
-      "--base-path",
-      basePath,
-      "--chain",
-      chain,
-      "--port",
-      `${port}`,
-      "--ws-port",
-      `${wsPort}`,
-      "--node-key",
-      nodeKey,
-      "--",
-      "--execution",
-      "wasm",
-      "--chain",
-      relayChain,
-      "--bootnodes",
-      relayBootnodes,
-    ],
-    stdout: "piped",
-    stderr: "piped",
-    signal,
-  }).spawn()
-
-  // TODO: redirect stdout/stderr to a file
-
-  // TODO: handle child early exit
-  // child.status.then(({ success }) => {
-  //   if (!success) {
-  //   }
-  // })
-
-  return { child, port, wsPort }
+  return await Promise.all(["state", "wasm"].map(async (type) => {
+    const { success, stdout } = await new Deno.Command(binary, {
+      args: [`export-genesis-${type}`, "--chain", chain],
+      signal,
+    }).output()
+    if (!success) {
+      // TODO: improve error message
+      throw new Error(`export-genesis-${type} failed`)
+    }
+    return new TextDecoder().decode(stdout)
+  })) satisfies string[] as [state: string, wasm: string]
 }
 
 function generateBootnodeString(port: number, peerId: string) {
   return `/ip4/127.0.0.1/tcp/${port}/p2p/${peerId}`
-}
-
-interface SpawnRelayNodeProps {
-  command: string
-  chain: string
-  keystoreAccount: string
-  basePath: string
-  bootnodes: string
-  signal: AbortSignal
-}
-
-function spawnRelayNode({
-  command,
-  chain,
-  keystoreAccount,
-  basePath,
-  bootnodes,
-  signal,
-}: SpawnRelayNodeProps) {
-  const [port, wsPort] = [getFreePort(), getFreePort()]
-  const child = new Deno.Command(command, {
-    args: [
-      "--validator",
-      `--${keystoreAccount}`,
-      "--base-path",
-      basePath,
-      "--chain",
-      chain,
-      "--port",
-      `${port}`,
-      "--ws-port",
-      `${wsPort}`,
-      "--bootnodes",
-      bootnodes,
-    ],
-    stdout: "piped",
-    stderr: "piped",
-    signal,
-  }).spawn()
-
-  // TODO: redirect stdout/stderr to a file
-
-  // TODO: handle child early exit
-  // child.status.then(({ success }) => {
-  //   if (!success) {
-  //   }
-  // })
-
-  return { child, port, wsPort }
-}
-
-interface SpawnRelayBootNodeProps {
-  command: string
-  chain: string
-  keystoreAccount: string
-  basePath: string
-  nodeKey: string
-  signal: AbortSignal
-}
-
-function spawnRelayBootNode({
-  command,
-  chain,
-  keystoreAccount,
-  basePath,
-  nodeKey,
-  signal,
-}: SpawnRelayBootNodeProps) {
-  const [port, wsPort] = [getFreePort(), getFreePort()]
-  const child = new Deno.Command(command, {
-    args: [
-      "--validator",
-      `--${keystoreAccount}`,
-      "--base-path",
-      basePath,
-      "--chain",
-      chain,
-      "--port",
-      `${port}`,
-      "--ws-port",
-      `${wsPort}`,
-      "--node-key",
-      nodeKey,
-    ],
-    stdout: "piped",
-    stderr: "piped",
-    signal,
-  }).spawn()
-
-  // TODO: redirect stdout/stderr to a file
-
-  // TODO: handle child early exit
-  // child.status.then(({ success }) => {
-  //   if (!success) {
-  //   }
-  // })
-
-  return { child, port, wsPort }
 }
 
 interface ChainSpec {
@@ -443,4 +186,77 @@ async function createCustomChainSpec<T>(
   await Deno.writeFile(rawPath, rawResult.stdout)
 
   return rawPath
+}
+
+const keystoreAccounts = ["alice", "bob", "charlie", "dave", "eve", "ferdie", "one", "two"]
+async function spawnNodes(
+  dir: string,
+  binary: string,
+  chain: string,
+  count: number,
+  extraArgs: string[],
+  signal: AbortSignal,
+) {
+  let bootnodes: string | undefined
+
+  for (let i = 0; i < count; i++) {
+    const keystoreAccount = keystoreAccounts[i]
+    if (!keystoreAccount) throw new Error("ran out of keystore accounts")
+    const nodeDir = path.join(dir, keystoreAccount)
+    await Deno.mkdir(nodeDir, { recursive: true })
+    const port = getFreePort()
+    const wsPort = getFreePort()
+    const args = [
+      "--validator",
+      `--${keystoreAccount}`,
+      "--base-path",
+      nodeDir,
+      "--chain",
+      chain,
+      "--port",
+      `${port}`,
+      "--ws-port",
+      `${wsPort}`,
+    ]
+    if (bootnodes) {
+      args.push("--bootnodes", bootnodes)
+    } else {
+      const { nodeKey, peerId } = await generateNodeKey(binary)
+      args.push("--node-key", nodeKey)
+      bootnodes = generateBootnodeString(port, peerId)
+    }
+    args.push(...extraArgs)
+    spawnNode(nodeDir, binary, args, signal)
+    console.log(binary, keystoreAccount, wsPort)
+  }
+
+  if (!bootnodes) throw new Error("count must be > 1")
+  return bootnodes
+}
+
+async function spawnNode(dir: string, binary: string, args: string[], signal: AbortSignal) {
+  const child = new Deno.Command(binary, {
+    args,
+    signal,
+    stdout: "piped",
+    stderr: "piped",
+  }).spawn()
+
+  child.stdout.pipeTo(
+    writableStreamFromWriter(
+      await Deno.open(path.join(dir, "stdout"), { write: true, create: true }),
+    ),
+  )
+
+  child.stderr.pipeTo(
+    writableStreamFromWriter(
+      await Deno.open(path.join(dir, "stderr"), { write: true, create: true }),
+    ),
+  )
+
+  child.status.then((status) => {
+    if (!signal.aborted) {
+      throw new Error(`process exited with code ${status.code}`)
+    }
+  })
 }
