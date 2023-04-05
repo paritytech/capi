7e9e0688607ea98b056c7ceac7e7eae2d67aec42
Harry Solovay <harrysolovay@gmail.com>
2023-04-05 13:52:26 -0400

    clean up intro

--- examples/rune/intro.ts
+++ examples/rune/intro.ts
@@ -1,7 +1,7 @@
 /**
  * @title Rune Intro
  * @stability nearing
- * @description Rune is our unit of composition in Capi. It can be thought of
+ * @description Runes are our units of composition in Capi. A Rune can be thought of
  * as an interaction *component*. It is a lazy description of our data requirements.
  * Runes can model storage retrievals, conditional transaction pipelines and more.
  */
@@ -15,15 +15,18 @@ const value = Rune.constant(3)
 // Map over `value` and multiply it by itself. Then, run it.
 const a = await value.map((value) => value * value).run()
 
-// Ensure `a` is the expected number.
+// Ensure `a` is as expected.
+console.log("a:", a)
 assertEquals(a, 9)
 
 // Now lets create a Rune factory (a function that accepts a given
 // runic value(s) and produces a new Rune). If this looks a bit
-// strange, note this is because of type-level handling, in which
-// any of the supplied arguments (which can be `T` or `Rune<T>`)
-// might encapsulate error type information, which must be appropriately
-// propagated to the factory-produced Rune.
+// strange, there's a reason: the consumer of this factory could
+// supply either a number or a Rune that resolves to a number.
+// If a numeric Rune is applied, that Rune might contain important
+// type information (more on this in [u_track](./u_track)). In the
+// case that there is in fact `U` type info, we want this info propagated
+// to the factory-produced Rune. Hence, we define Rune factories like so:
 function exp<X>(...[n]: RunicArgs<X, [n: number]>) {
   return Rune.resolve(n).map((n) => n * n)
 }
@@ -32,6 +35,8 @@ function exp<X>(...[n]: RunicArgs<X, [n: number]>) {
 // supply either a resolved value `3` or another Rune, which resolves
 // to the factory's parameter type.
 const b = await exp(3).run()
+console.log("b:", b)
 assertEquals(b, 9)
 const c = await exp(value).run()
+console.log("c:", c)
 assertEquals(c, 9)
