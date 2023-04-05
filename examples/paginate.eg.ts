a84a4c5ea5b051b6a9421dc27cb14bfb78f959e7
Harry Solovay <harrysolovay@gmail.com>
2023-04-05 08:03:01 -0400

    update paginate example to use dev chain

--- examples/paginate.eg.ts
+++ examples/paginate.eg.ts
@@ -6,8 +6,7 @@
  */
 
 import { $ } from "capi"
-import { System } from "polkadot_dev/mod.js"
-import { types, Uniques } from "statemint/mod.js"
+import { System, types } from "polkadot_dev/mod.js"
 
 // Reference the first 10 keys of a polkadot dev chain's system account map.
 const accountKeys = await System.Account.keyPage(10, null).run()
@@ -16,12 +15,12 @@ const accountKeys = await System.Account.keyPage(10, null).run()
 console.log("Account keys:", accountKeys)
 $.assert($.sizedArray($.uint8Array, 10), accountKeys)
 
-// Reference the first 10 key-value pairs of a statemint dev chain's uniques class map.
-const collectionsEntries = await Uniques.Class.entryPage(10, null).run()
+// Reference the first 10 key-value pairs of a polkadot dev chain's system account map.
+const accountEntries = await System.Account.entryPage(10, null).run()
 
-// Each entry should be of type `[number, types.pallet_uniques.types.CollectionDetails]`
-console.log("Collections entries:", collectionsEntries)
+// Each entry should be of type `[Uint8Array, AccountInfo]`
+console.log("Account entries:", accountEntries)
 $.assert(
-  $.sizedArray($.tuple($.u32, types.pallet_uniques.types.$collectionDetails), 10),
-  collectionsEntries,
+  $.sizedArray($.tuple($.uint8Array, types.frame_system.$accountInfo), 10),
+  accountEntries,
 )
