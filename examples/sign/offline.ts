56743dec99a76d859e20d83483ab8006d0c1b6b6
Harry Solovay <harrysolovay@gmail.com>
2023-04-06 18:14:07 -0400

    feat: add `SignedExtrinsicRune` `from` and `fromHex` methods (#853)

---
+++ examples/sign/offline.ts
@@ -0,0 +1,43 @@
+/**
+ * @title Offline Signing
+ * @stability nearing
+ * @description Create and sign an extrinsic, then serialize it into a hex for later use.
+ * Finally, rehydrate the extrinsic and submit it.
+ */
+
+import { $, SignedExtrinsicRune } from "capi"
+import { signature } from "capi/patterns/signature/polkadot.ts"
+import { Balances, chain, createUsers } from "westend_dev/mod.js"
+
+const { alexa, billy } = await createUsers()
+
+// Create and sign the extrinsic. Extract the hex.
+const hex = await Balances
+  .transfer({
+    value: 12345n,
+    dest: billy.address,
+  })
+  .signed(signature({ sender: alexa }))
+  .hex()
+  .run()
+
+// Save `hex` however you'd like (potentially sending to a relayer service,
+// writing to disk, etc.).
+save(hex)
+
+// Hydrate the signed extrinsic, submit it and await finalization.
+const hash = await SignedExtrinsicRune
+  .fromHex(chain, hex)
+  .sent()
+  .dbgStatus("Tx status:")
+  .finalized()
+  .run()
+
+// Ensure the extrinsic has been finalized.
+$.assert($.str, hash)
+
+// egdoc-ignore-start
+// The following noop is solely for explanation. Swap this out with your
+// own signed-hex-representation-consuming code.
+function save(_hex: string) {}
+// egdoc-ignore-end
