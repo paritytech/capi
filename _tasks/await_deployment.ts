c20908dbfd2e4bc8a4e8f8d3e3c43b0a2ef9c2db
tjjfvi <t6@t6.fyi>
2023-04-07 10:29:19 -0400

    use delay in await_deployment

--- _tasks/await_deployment.ts
+++ _tasks/await_deployment.ts
@@ -1,3 +1,4 @@
+import { delay } from "../deps/std/async.ts"
 import { _getDeploymentUrl } from "../server/capi.dev/delegator.ts"
 
 const sha = Deno.env.get("CAPI_SHA")!
@@ -5,5 +6,5 @@ const sha = Deno.env.get("CAPI_SHA")!
 const deploymentPollInterval = 5_000
 
 while (!(await _getDeploymentUrl(sha))) {
-  await new Promise((r) => setTimeout(r, deploymentPollInterval))
+  await delay(deploymentPollInterval)
 }
