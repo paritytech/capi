004c76b30ad8979e0209db515dc1aad75fae8f0e
Matias Volpe <kratico@users.noreply.github.com>
2023-04-11 15:40:50 -0300

    feat: archive devnet stdout/stderr on workflow failure (#870)
    Co-authored-by: T6 <t6@t6.fyi>

--- devnets/createTempDir.ts
+++ devnets/createTempDir.ts
@@ -1,7 +1,9 @@
 import * as path from "../deps/std/path.ts"
 
 export async function createTempDir() {
-  const dir = path.resolve(`target/devnets/devnet-${new Date().toISOString()}`)
+  const dir = path.resolve(
+    `target/devnets/devnet-${new Date().toISOString().replace(/[:T.]/g, "-").slice(0, -1)}`,
+  )
   await Deno.mkdir(dir, { recursive: true })
   return dir
 }
