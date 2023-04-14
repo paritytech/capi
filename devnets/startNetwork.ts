48d89b8093a155b4afaa9695b015cf3720cf8fe4
Matias Volpe <matias@parity.io>
2023-04-14 13:43:52 -0300

    fix: spell check

--- devnets/startNetwork.ts
+++ devnets/startNetwork.ts
@@ -221,6 +221,7 @@ const authorities = [
     srAccount: "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
     srStash: "5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY",
     edAccount: "5FA9nQDVg267DEd8m1ZypXLBnvN7SFxYwV7ndqSYGiN9TTpu",
+    // cspell:disable-next-line
     ecAccount: "KW39r9CJjAVzmkf9zQ4YDb2hqfAVGdRqn53eRqyruqpxAP5YL",
   },
   {
@@ -228,6 +229,7 @@ const authorities = [
     srAccount: "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
     srStash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
     edAccount: "5GoNkf6WdbxCFnPdAnYYQyCjAKPJgLNxXwPjwTh6DGg6gN3E",
+    // cspell:disable-next-line
     ecAccount: "KWByAN7WfZABWS5AoWqxriRmF5f2jnDqy3rB5pfHLGkY93ibN",
   },
   {
@@ -235,6 +237,7 @@ const authorities = [
     srAccount: "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
     srStash: "5Ck5SLSHYac6WFt5UZRSsdJjwmpSZq85fd5TRNAdZQVzEAPT",
     edAccount: "5DbKjhNLpqX3zqZdNBc9BGb4fHU1cRBaDhJUskrvkwfraDi6",
+    // cspell:disable-next-line
     ecAccount: "KWBpGtyJLBkJERdZT1a1uu19c2uPpZm9nFd8SGtCfRUAT3Y4w",
   },
   {
@@ -242,6 +245,7 @@ const authorities = [
     srAccount: "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy",
     srStash: "5HKPmK9GYtE1PSLsS1qiYU9xQ9Si1NcEhdeCq9sw5bqu4ns8",
     edAccount: "5ECTwv6cZ5nJQPk6tWfaTrEk8YH2L7X1VT4EL5Tx2ikfFwb7",
+    // cspell:disable-next-line
     ecAccount: "KWCycezxoy7MWTTqA5JDKxJbqVMiNfqThKFhb5dTfsbNaGbrW",
   },
   {
@@ -249,6 +253,7 @@ const authorities = [
     srAccount: "5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",
     srStash: "5FCfAonRZgTFrTd9HREEyeJjDpT397KMzizE6T3DvebLFE7n",
     edAccount: "5Ck2miBfCe1JQ4cY3NDsXyBaD6EcsgiVmEFTWwqNSs25XDEq",
+    // cspell:disable-next-line
     ecAccount: "KW9NRAHXUXhBnu3j1AGzUXs2AuiEPCSjYe8oGan44nwvH5qKp",
   },
   {
@@ -256,6 +261,7 @@ const authorities = [
     srAccount: "5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL",
     srStash: "5CRmqmsiNFExV6VbdmPJViVxrWmkaXXvBrSX8oqBT8R9vmWk",
     edAccount: "5E2BmpVFzYGd386XRCZ76cDePMB3sfbZp5ZKGUsrG1m6gomN",
+    // cspell:disable-next-line
     ecAccount: "KW6E1KGr5pqJ9Trgt7eAuA7d7mgpJPydiEDKc2h1aGTEEzYC1",
   },
 ] as const
@@ -263,7 +269,8 @@ function addAuthorities(genesisConfig: GenesisConfig, count: number) {
   if (count > authorities.length) {
     throw new Error(`authorities count should be <= ${authorities.length}`)
   }
-  if (!genesisConfig.session) throw new Error(`pallet_session is not configured`)
+  // TODO: #889 add support for pallet_session, pallet_aura and pallet_grandpa
+  if (!genesisConfig.session) return
   genesisConfig.session.keys.length = 0
   authorities.slice(0, count).forEach(({ srAccount, srStash, edAccount, ecAccount }) =>
     genesisConfig.session!.keys.push([
