23767a3181edf436bd641b6638cf73f2bc1a9eb9
Harry Solovay <harrysolovay@gmail.com>
2023-04-14 11:24:31 -0400

    enable watch example

--- examples/watch.eg.ts
+++ examples/watch.eg.ts
@@ -4,7 +4,6 @@
  * @description Capi allows you to model your data requirements as Runes and
  * then utilize `watch`, which produces an async generator, the pulls of which
  * produce promises resolving to subsequent states.
- * @test_skip
  */
 
 import { chain, Timestamp } from "@capi/polkadot"
