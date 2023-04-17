a988be110bd777857754deff1e916e1ac857d0fe
tjjfvi <t6@t6.fyi>
2023-04-17 11:11:02 -0400

    incorporate chain spec retrieval as example

---
+++ examples/smoldot/smoldot.eg.ts
@@ -0,0 +1,24 @@
+/**
+ * @title Smoldot Connection
+ * @stability nearing
+ * @description Smoldot enables users to run a tiny node in their browser,
+ * such that they can connect directly to the network(s) without going through
+ * a centralized intermediary. This is the future of unstoppable applications.
+ */
+
+import { chain } from "@capi/polkadot-dev"
+import { $, known, SmoldotConnection } from "capi"
+
+// Bring the chainspec into scope. Here, we'll import it from a local file.
+import relayChainSpec from "./chainspec.json" assert { type: "json" }
+
+// Initialize a `ChainRune` with `SmoldotConnection` and the chainspec.
+const { block } = await chain
+  .with(SmoldotConnection.bind({ relayChainSpec: JSON.stringify(relayChainSpec) }))
+  .blockHash()
+  .block()
+  .run()
+
+// Ensure the block is of the expected shape.
+console.log(block)
+$.assert(known.$block, block)
