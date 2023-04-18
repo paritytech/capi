908853c54560f47c0c665990ab51f6207adeb7b2
T6 <t6@t6.fyi>
2023-04-18 10:13:10 -0400

    update cbb

--- deps/capi_binary_builds.ts
+++ deps/capi_binary_builds.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/paritytech/capi-binary-builds/6c2c5ab/download.ts"
+export * from "https://raw.githubusercontent.com/paritytech/capi-binary-builds/c794b02/download.ts"
