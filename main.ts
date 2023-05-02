62a71816c003e479692bf57836d639804a3b66a8
Harry Solovay <harrysolovay@gmail.com>
2023-05-02 17:14:22 -0400

    add bin back to main

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import "./deps/shims/register.ts"
 
+import bin from "./cli/bin.ts"
 import serve from "./cli/serve.ts"
 import sync from "./cli/sync.ts"
 
-const commands: Record<string, (...args: string[]) => void> = { serve, sync }
+const commands: Record<string, (...args: string[]) => void> = { bin, serve, sync }
 
 if (Deno.args[0]! in commands) {
   commands[Deno.args[0]!]!(...Deno.args.slice(1))
