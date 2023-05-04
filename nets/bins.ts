cff6cfb075810ad43af3ec85c01d634b0b1aa1a9
Harry Solovay <harrysolovay@gmail.com>
2023-05-04 07:06:48 -0400

    use map entries

--- nets/bins.ts
+++ nets/bins.ts
@@ -1,4 +1,5 @@
 import { CapiBinary } from "../deps/capi_binary_builds.ts"
+import { mapEntries } from "../deps/std/collections/map_entries.ts"
 import { AsyncMemo } from "../util/memo.ts"
 
 export type CapiBinaryArgs = ConstructorParameters<typeof CapiBinary>
@@ -14,19 +15,17 @@ export function bins<K extends PropertyKey>(
 export function bins(
   binariesProps: Record<string, string | CapiBinaryArgs>,
 ): Record<string, BinaryResolver> {
-  return Object.fromEntries(
-    Object.entries(binariesProps).map(([k, v]) => {
-      if (typeof v === "string") return [k, () => Promise.resolve(v)]
-      const binary = new CapiBinary(...v as CapiBinaryArgs)
-      const key = `${v[0]}@${v[1]}`
-      return [k, (_signal: AbortSignal) =>
-        memo.run(key, async () => {
-          if (!(await binary.exists())) {
-            console.log("Downloading", key)
-            await binary.download()
-          }
-          return binary.path
-        })]
-    }),
-  )
+  return mapEntries(binariesProps, ([k, v]) => {
+    if (typeof v === "string") return [k, () => Promise.resolve(v)]
+    const binary = new CapiBinary(...v as CapiBinaryArgs)
+    const key = `${v[0]}@${v[1]}`
+    return [k, (_signal: AbortSignal) =>
+      memo.run(key, async () => {
+        if (!(await binary.exists())) {
+          console.log("Downloading", key)
+          await binary.download()
+        }
+        return binary.path
+      })]
+  })
 }
