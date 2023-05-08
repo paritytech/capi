582545b6c11182dd77bc7e30f612da2674d19453
Matias Volpe <matias@parity.io>
2023-05-08 12:37:39 -0300

    feat: use default nets path for resolveNets

--- cli/resolveNets.ts
+++ cli/resolveNets.ts
@@ -4,8 +4,8 @@ import { NetSpec } from "../nets/mod.ts"
 
 const $nets = $.record($.instance(NetSpec as new() => NetSpec, $.tuple(), () => []))
 
-export async function resolveNets(netsPathRaw?: string): Promise<Record<string, NetSpec>> {
-  const netsPath = path.resolve(netsPathRaw ?? "./nets.ts")
+export async function resolveNets(netsPathRaw = "./nets.ts"): Promise<Record<string, NetSpec>> {
+  const netsPath = path.resolve(netsPathRaw)
   await Deno.stat(netsPath)
   const nets = await import(path.toFileUrl(netsPath).toString())
   $.assert($nets, nets)
