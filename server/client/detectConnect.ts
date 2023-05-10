6f52866ef81c5f99225e2b36f319156f275b6322
Harry Solovay <harrysolovay@gmail.com>
2023-05-10 10:29:40 -0400

    cleanup

--- server/client/detectConnect.ts
+++ server/client/detectConnect.ts
@@ -3,21 +3,18 @@ import { Connect, WsConnection } from "../../rpc/mod.ts"
 import { DevnetConnection } from "./DevnetConnection.ts"
 
 export function detectConnect(
-  spec: ConnectionSpec | undefined,
+  spec: ConnectionSpec,
   targets: Record<string, ConnectionSpec>,
-): Connect | undefined {
+): Connect {
   const targetName = Deno.env.get("CAPI_TARGET")
   if (targetName) {
     const target = targets[targetName]
     if (target) spec = target
   }
-  if (spec) {
-    switch (spec.type) {
-      case "DevnetConnection":
-        return DevnetConnection.bind(spec.discovery)
-      case "WsConnection":
-        return WsConnection.bind(spec.discovery)
-    }
+  switch (spec.type) {
+    case "DevnetConnection":
+      return DevnetConnection.bind(spec.discovery)
+    case "WsConnection":
+      return WsConnection.bind(spec.discovery)
   }
-  return
 }
