bb402919313f87176a1f5c02d288f03af2e1eac5
Harry Solovay <harrysolovay@gmail.com>
2023-05-17 05:20:17 -0400

    fixes

--- server/detectVersion.node.ts
+++ server/detectVersion.node.ts
@@ -1,8 +1,8 @@
-export async function detectVersion() {
+export function detectVersion(): string {
   const packageJsonPath = new URL("../package.json", import.meta.url)
   try {
-    await Deno.stat(packageJsonPath)
-    const packageJson = await Deno.readTextFile(packageJsonPath)
+    Deno.statSync(packageJsonPath)
+    const packageJson = Deno.readTextFileSync(packageJsonPath)
     return JSON.parse(packageJson).version as string
   } catch (_e) {
     throw new Error(`Could not resolve \`package.json\`.`)
--- server/detectVersion.ts
+++ server/detectVersion.ts
@@ -1,7 +1,7 @@
 const rCapiVersion = /^https:\/\/(?:capi.dev\/|deno.land\/x\/capi)@(.+?)\//
 
 export function detectVersion(): string {
-  const version = rCapiVersion.exec(import.meta.url)?.[0]
+  const version = rCapiVersion.exec(import.meta.url)?.[1]
   if (!version) {
     throw new Error("Could not detect version from URL. Please specify via `--version` flag.")
   }
