13e70997d864f8e2c050e07dbddb585a01826ee4
Harry Solovay <harrysolovay@gmail.com>
2023-05-18 10:25:27 -0400

    Update server/detectVersion.ts
    Co-authored-by: T6 <t6@t6.fyi>

--- server/detectVersion.ts
+++ server/detectVersion.ts
@@ -3,9 +3,12 @@ const rCapiVersion = /^https:\/\/(?:capi.dev\/|deno.land\/x\/capi)@(.+?)\//
 export function detectVersion(): string | undefined {
   const match = rCapiVersion.exec(import.meta.url)?.[1]
   if (match) return match
-  const sha = new TextDecoder().decode(
-    new Deno.Command("git", { args: ["rev-parse", "HEAD"] }).outputSync().stdout,
-  )
-  if (sha.length) return sha.slice(0, 8)
+  if (new URL(import.meta.url).protocol === "file:") {
+    const sha = new TextDecoder().decode(
+      new Deno.Command("git", { args: ["rev-parse", "HEAD"] 
+ }).outputSync().stdout,
+    )
+    if (sha.length) return sha.slice(0, 8)
+  }
   return
 }
