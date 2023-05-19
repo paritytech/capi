5f8e8dd26efcf7f6f2ba85b9208e59e3b622d23a
Harry Solovay <harrysolovay@gmail.com>
2023-05-19 10:33:17 -0400

    t6 feedback

--- server/detectVersion.ts
+++ server/detectVersion.ts
@@ -1,3 +1,5 @@
+import * as path from "../deps/std/path.ts"
+
 const rCapiVersion = /^https:\/\/(?:capi.dev\/|deno.land\/x\/capi)@(.+?)\//
 
 export function detectVersion(): string | undefined {
@@ -5,7 +7,10 @@ export function detectVersion(): string | undefined {
   if (match) return match
   if (new URL(import.meta.url).protocol === "file:") {
     const sha = new TextDecoder().decode(
-      new Deno.Command("git", { args: ["rev-parse", "HEAD"] }).outputSync().stdout,
+      new Deno.Command("git", {
+        args: ["rev-parse", "HEAD"],
+        cwd: path.fromFileUrl(new URL(".", import.meta.url)),
+      }).outputSync().stdout,
     )
     if (sha.length) return sha.slice(0, 8)
   }
