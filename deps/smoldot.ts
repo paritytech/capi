aeaa2cc80fd282661e6efe6a2d9aecd7bb8803f1
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2023-05-23 17:14:21 +0200

    style: run `dprint fmt`

--- deps/smoldot.ts
+++ deps/smoldot.ts
@@ -1,5 +1,5 @@
+export * from "https://deno.land/x/smoldot2@light-js-deno-v1.0.6/index-deno.js"
 export type {
   Client,
   ClientOptions,
-} from "https://deno.land/x/smoldot2@light-js-deno-v1.0.6/public-types.d.ts";
-export * from "https://deno.land/x/smoldot2@light-js-deno-v1.0.6/index-deno.js";
+} from "https://deno.land/x/smoldot2@light-js-deno-v1.0.6/public-types.d.ts"
