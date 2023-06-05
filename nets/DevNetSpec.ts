22e4015bd3a25bb470f4d52ead055375936922d1
Matias Volpe <matias@parity.io>
2023-06-05 11:51:20 -0300

    feat: address PR comments

--- nets/DevNetSpec.ts
+++ nets/DevNetSpec.ts
@@ -106,8 +106,11 @@ export async function spawnDevNet({
     if (!keystoreAccount) throw new Error("ran out of keystore accounts")
     const nodeDir = path.join(tempDir, keystoreAccount)
     await Deno.mkdir(nodeDir, { recursive: true })
-    const httpPort = await getFreePort()
-    const wsPort = await getFreePort()
+    const [httpPort, wsPort, wsPortArgName_] = await Promise.all([
+      getFreePort(),
+      getFreePort(),
+      wsPortArgName(binary),
+    ])
     ports.push(wsPort)
     const args = [
       "--validator",
@@ -118,7 +121,7 @@ export async function spawnDevNet({
       chainSpecPath,
       "--port",
       `${httpPort}`,
-      await getBinaryWsPortCliArg(binary),
+      wsPortArgName_,
       `${wsPort}`,
     ]
     if (bootnodes) {
