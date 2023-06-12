2f867227a49587300c3f872658dc464adf70c3f5
Harry Solovay <harrysolovay@gmail.com>
2023-06-12 19:16:53 -0400

    remove compression reexport from util/mod.ts

--- util/mod.ts
+++ util/mod.ts
@@ -1,9 +1,8 @@
-// moderate --exclude cache port.ts proxyWebSocket.ts tsFormatter.ts
+// moderate --exclude compression.ts cache port.ts proxyWebSocket.ts tsFormatter.ts
 
 export * from "./ArrayOfLength.ts"
 export * from "./bytes.ts"
 export * from "./clock.ts"
-export * from "./compression.ts"
 export * from "./gracefulExit.ts"
 export * from "./key.ts"
 export * from "./memo.ts"
