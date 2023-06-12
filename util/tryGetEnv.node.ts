046ef16a20d9544f8b9025ec21c15d3150c3169f
Harry Solovay <harrysolovay@gmail.com>
2023-06-12 19:13:28 -0400

    fix

--- util/tryGetEnv.node.ts
+++ util/tryGetEnv.node.ts
@@ -2,7 +2,7 @@ declare global {
   interface Process {
     env: Record<string, string>
   }
-  export const process?: Process
+  export const process: Process | undefined
 }
 export function tryGetEnv(name: string): string | undefined {
   return typeof process !== "undefined" ? process.env?.[name] : undefined
