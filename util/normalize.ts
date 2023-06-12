03c28a04a0f965e2cad5ce65ad87cf01b54af342
Matias Volpe <kratico@users.noreply.github.com>
2023-06-12 13:14:50 -0300

    Update util/normalize.ts
    Co-authored-by: T6 <t6@t6.fyi>

--- util/normalize.ts
+++ util/normalize.ts
@@ -21,7 +21,54 @@ export function normalizeVariableName(name: string) {
   return normalizeIdent(name).replace(/^./, (x) => x.toLowerCase())
 }
 
-const keywords = ["import"]
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#reserved_words
+const keywords = [
+  "break",
+  "case",
+  "catch",
+  "class",
+  "const",
+  "continue",
+  "debugger",
+  "delete",
+  "do",
+  "else",
+  "export",
+  "extends",
+  "false",
+  "finally",
+  "for",
+  "function",
+  "if",
+  "import",
+  "in",
+  "instanceof",
+  "new",
+  "null",
+  "return",
+  "super",
+  "switch",
+  "this",
+  "throw",
+  "true",
+  "try",
+  "typeof",
+  "var",
+  "void",
+  "while",
+  "with",
+  "let",
+  "static",
+  "yield",
+  "await",
+  "enum",
+  "implements",
+  "interface",
+  "package",
+  "private",
+  "protected",
+  "public",
+]
 function normalizeKeyword(ident: string) {
   return keywords.includes(ident) ? ident + "_" : ident
 }
