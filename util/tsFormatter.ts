99b762387d1a4eb8ab4002b57618a8207083a3da
tjjfvi <t6@t6.fyi>
2023-06-14 10:19:03 -0400

    compress ts formatter wasm

--- util/tsFormatter.ts
+++ util/tsFormatter.ts
@@ -1,7 +1,12 @@
 import { createFromBuffer } from "../deps/dprint.ts"
 import dprintConfig from "../dprint.json" assert { type: "json" }
-import { tsFormatterWasm } from "./_artifacts/tsFormatterWasm.ts"
+import { tsFormatterWasmCompressed } from "./_artifacts/tsFormatterWasmCompressed.ts"
+import { decompress } from "./compression.ts"
 
-export const tsFormatter = createFromBuffer(tsFormatterWasm)
-const { indentWidth, lineWidth, typescript: config } = dprintConfig
-tsFormatter.setConfig({ indentWidth, lineWidth }, config)
+export const tsFormatterPromise = (async () => {
+  const tsFormatterWasm = await decompress(tsFormatterWasmCompressed)
+  const tsFormatter = createFromBuffer(tsFormatterWasm)
+  const { indentWidth, lineWidth, typescript: config } = dprintConfig
+  tsFormatter.setConfig({ indentWidth, lineWidth }, config)
+  return tsFormatter
+})()
