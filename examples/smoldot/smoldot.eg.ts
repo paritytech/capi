d10f823a72272d43dc8bdbc37006db7377be0ca0
Harry Solovay <harrysolovay@gmail.com>
2023-06-14 11:01:46 -0400

    fix name

--- examples/smoldot/smoldot.eg.ts
+++ examples/smoldot/smoldot.eg.ts
@@ -6,14 +6,14 @@
  * a centralized intermediary. This is the future of unstoppable applications.
  */
 
-import { PolkadotDevRune } from "@capi/polkadot-dev"
+import { PolkadotRune } from "@capi/polkadot"
 import { $, known, SmoldotConnection } from "capi"
 
 /// Bring the chainspec into scope. Here, we'll import it from `fetch_chainspec.eg.ts`.
 import { relayChainSpec } from "./fetch_chainspec.eg.ts"
 
 /// Use the generate chain rune with `SmoldotConnection` and the chainspec.
-const polkadot = PolkadotDevRune.from(SmoldotConnection.bind({ relayChainSpec }))
+const polkadot = PolkadotRune.from(SmoldotConnection.bind({ relayChainSpec }))
 
 // Utilize the smoldot-connected `PolkadotRune` instance.
 const { block } = await polkadot.blockHash().block().run()
