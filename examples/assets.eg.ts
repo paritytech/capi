1540e6bf0ea5bda5f32800f4e69a6b88f6a44657
Ryan Lee <ryan@parity.io>
2023-06-15 18:20:36 -0400

    print billy balance

--- examples/assets.eg.ts
+++ examples/assets.eg.ts
@@ -6,9 +6,11 @@ const scope = new Scope()
 
 const { alexa, billy } = await createDevUsers()
 
+const ASSET_ID = 0
+
 // Create a new class of assets
 await rococoDevWestmint.Assets.create({
-  id: 0,
+  id: ASSET_ID,
   admin: alexa.address,
   minBalance: 1n,
 }).signed(signature({ sender: alexa }))
@@ -19,7 +21,7 @@ await rococoDevWestmint.Assets.create({
 
 // Mint 1000 units to Alexa
 await rococoDevWestmint.Assets.mint({
-  id: 0,
+  id: ASSET_ID,
   beneficiary: alexa.address,
   amount: 1000n,
 }).signed(signature({ sender: alexa }))
@@ -28,4 +30,20 @@ await rococoDevWestmint.Assets.mint({
   .finalized()
   .run(scope)
 
-// await rococoDevWestmint.Assets.transfer({})
+await rococoDevWestmint.Assets.transfer({
+  id: ASSET_ID,
+  target: billy.address,
+  amount: 10n,
+})
+  .signed(signature({ sender: alexa }))
+  .sent()
+  .dbgStatus("Transfer Asset to Billy:")
+  .finalized()
+  .run(scope)
+
+await rococoDevWestmint.Assets.Account
+  .value(Rune.tuple([ASSET_ID, billy.publicKey]))
+  .unhandle(is(undefined))
+  .map(({ balance }) => balance)
+  .dbg("Billy Asset Balance")
+  .run(scope)
