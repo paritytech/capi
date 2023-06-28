3ab55b4fe8b1e0355d2a21470b66bcffb15cb1ba
Matias Volpe <matias@parity.io>
2023-06-28 18:22:32 +0200

    feat: use bytes.equals

--- examples/kilt/did.eg.ts
+++ examples/kilt/did.eg.ts
@@ -1,5 +1,6 @@
 import { spiritnet } from "@capi/spiritnet"
 import { $, is } from "capi"
+import { equals } from "../../deps/std/bytes.ts"
 
 const { owner } = await spiritnet.Web3Names.Owner.value(
   new TextEncoder().encode("ingo"),
@@ -9,11 +10,7 @@ const { owner } = await spiritnet.Web3Names.Owner.value(
 const { authenticationKey, publicKeys } = await spiritnet.Did.Did.value(owner)
   .unhandle(is(undefined)).run()
 
-const value = [...publicKeys.entries()].find(([k, _v]) => bytesEqual(k, authenticationKey))
+const value = [...publicKeys.entries()].find(([k, _v]) => equals(k, authenticationKey))
 
 console.log("DID:", value![1].key.value)
 $.assert($.uint8Array, value![1].key.value.value)
-
-function bytesEqual(a: Uint8Array, b: Uint8Array) {
-  return a.every((x, i) => b[i] === x)
-}
