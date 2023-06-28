4fd37f0471705358cf08c11d324ef008f2a6cdd7
Matias Volpe <matias@parity.io>
2023-06-28 18:52:28 +0200

    feat: improve did example

--- examples/kilt/did.eg.ts
+++ examples/kilt/did.eg.ts
@@ -3,19 +3,23 @@
  * @description Get a user public key based on the web3name alias of a Kilt DID.
  */
 
-import { spiritnet } from "@capi/spiritnet"
+import { $didPublicKeyDetails, spiritnet } from "@capi/spiritnet"
 import { $, is } from "capi"
 import { equals } from "../../deps/std/bytes.ts"
 
-const { owner } = await spiritnet.Web3Names.Owner.value(
-  new TextEncoder().encode("ingo"),
-)
-  .unhandle(is(undefined)).run()
+const { owner } = await spiritnet.Web3Names.Owner
+  .value(new TextEncoder().encode("ingo"))
+  .unhandle(is(undefined))
+  .run()
 
-const { authenticationKey, publicKeys } = await spiritnet.Did.Did.value(owner)
-  .unhandle(is(undefined)).run()
+const [_, didPublicKeyDetails] = await spiritnet.Did.Did
+  .value(owner)
+  .unhandle(is(undefined))
+  .map(({ authenticationKey, publicKeys }) =>
+    [...publicKeys.entries()].find(([k, _v]) => equals(k, authenticationKey))
+  )
+  .unhandle(is(undefined))
+  .run()
 
-const value = [...publicKeys.entries()].find(([k, _v]) => equals(k, authenticationKey))
-
-console.log("DID:", value![1].key.value)
-$.assert($.uint8Array, value![1].key.value.value)
+console.log("DID public key details:", didPublicKeyDetails)
+$.assert($didPublicKeyDetails, didPublicKeyDetails)
