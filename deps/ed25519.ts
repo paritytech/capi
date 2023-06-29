7ef272c5caed81c3e448f3461f8845701de39310
T6 <t6@t6.fyi>
2023-06-29 11:12:16 +0200

    fix: capi node on TS/JS & CJS/ESM (#1115)

--- deps/ed25519.ts
+++ deps/ed25519.ts
@@ -1,1 +1,3 @@
-export * from "https://esm.sh/@noble/ed25519@2.0.0"
+// Force dnt to not use npm package as it does not support cjs
+// @deno-types="https://esm.sh/v117/@noble/ed25519@2.0.0/index.d.ts"
+export * from "./shims/ed25519.js"
---
+++ deps/shims/register-ts-node.ts
@@ -0,0 +1,3 @@
+export async function register() {
+  // no-op in Deno
+}
