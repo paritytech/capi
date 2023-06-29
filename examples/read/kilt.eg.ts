7febb581fa2dfff7656cd461407b34f5b03abcea
Matias Volpe <matias@parity.io>
2023-06-29 13:42:21 +0200

    feat: use map.get

--- examples/read/kilt.eg.ts
+++ examples/read/kilt.eg.ts
@@ -4,19 +4,16 @@
 
 import { $didPublicKeyDetails, spiritnet } from "@capi/spiritnet"
 import { $, is, ValueRune } from "capi"
-import { equals } from "../../deps/std/bytes.ts"
 
 const owner = spiritnet.Web3Names.Owner
   .entries({ limit: 1 })
   .into(ValueRune)
   .access(0, 1, "owner")
 
-const [_, didPublicKeyDetails] = await spiritnet.Did.Did
+const didPublicKeyDetails = await spiritnet.Did.Did
   .value(owner)
   .unhandle(is(undefined))
-  .map(({ authenticationKey, publicKeys }) =>
-    [...publicKeys.entries()].find(([k, _v]) => equals(k, authenticationKey))
-  )
+  .map(({ authenticationKey, publicKeys }) => publicKeys.get(authenticationKey))
   .unhandle(is(undefined))
   .run()
 
