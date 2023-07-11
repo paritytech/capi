427161f6aa8f20417dba6e002a6fdddada2a8dda
Ryan Lee <ryan@parity.io>
2023-07-11 16:36:13 -0400

    comments

--- examples/multisig/hydrate.eg.ts
+++ examples/multisig/hydrate.eg.ts
@@ -1,33 +1,36 @@
 /**
- * @title Serialization and Hydration
- * @description Creates a multisig, serializes it, and then hydrates it.
+ * @title Multisig Hydration
+ * @description Creates a multisig and encodes it into a hex string. This hex
+ * string is then used to hydrate a new MultisigRune. This is useful if you
+ * have created a multisig and want to use it a later date or you want to send
+ * it other signatories.
  */
 
 import { polkadotDev } from "@capi/polkadot-dev"
-import { assertArrayIncludes, assertEquals } from "asserts"
-import { createDevUsers, ValueRune } from "capi"
+import { assertEquals } from "asserts"
+import { createDevUsers } from "capi"
 import { MultisigRune } from "capi/patterns/multisig"
 
 const { alexa, billy, carol } = await createDevUsers()
 
-/// Create the multisig. Extract the hex.
-const hex = await MultisigRune.from(polkadotDev, {
+const multisigParams = {
   signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
   threshold: 2,
-}).hex.run()
+}
+/// Create the multisig. Extract the hex.
+const multisig = MultisigRune.from(polkadotDev, multisigParams)
 
+assertEquals(await multisig.run(), multisigParams)
+
+const hex = await multisig.hex.run()
 /// Save `hex` however you'd like (i.e. writing to disk, etc.).
 save(hex)
 
 // Hydrate the multisig
-const multisig = MultisigRune.fromHex(polkadotDev, hex)
-
-const { signatories, threshold } = await multisig.into(ValueRune).run()
+const hydratedMultisig = MultisigRune.fromHex(polkadotDev, hex)
 
 // Test hydration
-assertEquals(signatories.length, 3)
-assertArrayIncludes(signatories, [alexa.publicKey, billy.publicKey, carol.publicKey])
-assertEquals(threshold, 2)
+assertEquals(await hydratedMultisig.run(), multisigParams)
 
 // hide-start
 // The following noop is solely for explanation. Swap this out with your
