19d3ccf5a499e20819abdf147179ac77db6cffba
Ryan Lee <ryan@parity.io>
2023-07-11 17:17:14 -0400

    fix: multisig hydrate documentation

--- examples/multisig/hydrate.eg.ts
+++ examples/multisig/hydrate.eg.ts
@@ -2,8 +2,7 @@
  * @title Multisig Hydration
  * @description Creates a multisig and encodes it into a hex string. This hex
  * string is then used to hydrate a new MultisigRune. This is useful if you
- * have created a multisig and want to use it a later date or you want to send
- * it other signatories.
+ * want to use it a later date or if you want to send it to the other signatories.
  */
 
 import { polkadotDev } from "@capi/polkadot-dev"
@@ -11,6 +10,7 @@ import { assertEquals } from "asserts"
 import { createDevUsers } from "capi"
 import { MultisigRune } from "capi/patterns/multisig"
 
+/// Create three dev users. These users will serve as signatories of the multisig.
 const { alexa, billy, carol } = await createDevUsers()
 
 /// Create the multisig.
@@ -25,14 +25,15 @@ const hex = await multisig.hex.run()
 /// Save `hex` however you'd like (i.e. writing to disk, etc.).
 save(hex)
 
-// Hydrate a multisig with the hex from before.
+/// Hydrate a multisig with the hex from before.
 const hydratedMultisig = MultisigRune.fromHex(polkadotDev, hex)
 
-// Test hydration
+/// Ensure the initially-constructed multisig state is identical to that of
+/// the hydrated multisig.
 assertEquals(await multisig.run(), await hydratedMultisig.run())
 
 // hide-start
 // The following noop is solely for explanation. Swap this out with your
-// own signed-hex-representation-consuming code.
+// own hex-representation-consuming code.
 function save(_hex: string) {}
 // hide-end
