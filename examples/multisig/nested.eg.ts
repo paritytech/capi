5676e067b828aa3d5e849eb83796d3d276f25c87
Ryan Lee <ryan@parity.io>
2023-07-11 16:03:46 -0400

    formatting

--- examples/multisig/nested.eg.ts
+++ examples/multisig/nested.eg.ts
@@ -12,30 +12,34 @@ import { signature } from "capi/patterns/signature/polkadot"
 const { alexa, billy, carol, david, ellie } = await createDevUsers()
 
 // Create first multisig.
-const multisigA = MultisigRune.from(polkadotDev, {
-  signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
-  threshold: 2,
-})
+const multisigA = MultisigRune
+  .from(polkadotDev, {
+    signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
+    threshold: 2,
+  })
 
 // Create second multisig with first multisig as a signatory
-const multisigB = MultisigRune.from(
-  polkadotDev,
-  Rune.object({
-    signatories: Rune.array([
-      ...[david, ellie].map(({ publicKey }) => publicKey),
-      multisigA.accountId,
-    ]),
-    threshold: 2,
-  }),
-)
+const multisigB = MultisigRune
+  .from(
+    polkadotDev,
+    Rune.object({
+      signatories: Rune.array([
+        ...[david, ellie].map(({ publicKey }) => publicKey),
+        multisigA.accountId,
+      ]),
+      threshold: 2,
+    }),
+  )
 
 // Fund both multisigs
-await polkadotDev.Utility.batchAll({
-  calls: Rune.array([
-    multisigA.fund(2_000_000_000_000n),
-    multisigB.fund(2_000_000_000_000n),
-  ]),
-}).signed(signature({ sender: alexa }))
+await polkadotDev.Utility
+  .batchAll({
+    calls: Rune.array([
+      multisigA.fund(2_000_000_000_000n),
+      multisigB.fund(2_000_000_000_000n),
+    ]),
+  })
+  .signed(signature({ sender: alexa }))
   .sent()
   .dbgStatus(`Existential Deposits`)
   .finalized()
