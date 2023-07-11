3d318edeccdc9267bb3a3c3547096979f2e79797
Ryan Lee <ryan@parity.io>
2023-07-11 16:20:55 -0400

    revert formatting

--- examples/multisig/nested.eg.ts
+++ examples/multisig/nested.eg.ts
@@ -12,24 +12,22 @@ import { signature } from "capi/patterns/signature/polkadot"
 const { alexa, billy, carol, david, ellie } = await createDevUsers()
 
 // Create first multisig.
-const multisigA = MultisigRune
-  .from(polkadotDev, {
-    signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
-    threshold: 2,
-  })
+const multisigA = MultisigRune.from(polkadotDev, {
+  signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
+  threshold: 2,
+})
 
 // Create second multisig with first multisig as a signatory
-const multisigB = MultisigRune
-  .from(
-    polkadotDev,
-    Rune.object({
-      signatories: Rune.array([
-        ...[david, ellie].map(({ publicKey }) => publicKey),
-        multisigA.accountId,
-      ]),
-      threshold: 2,
-    }),
-  )
+const multisigB = MultisigRune.from(
+  polkadotDev,
+  Rune.object({
+    signatories: Rune.array([
+      ...[david, ellie].map(({ publicKey }) => publicKey),
+      multisigA.accountId,
+    ]),
+    threshold: 2,
+  }),
+)
 
 // Fund both multisigs
 await polkadotDev.Utility
