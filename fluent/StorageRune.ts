a7af34425231a3725d54358a31c6d9785116c8bb
Harry Solovay <harrysolovay@gmail.com>
2023-07-12 11:45:26 -0400

    prevent error when no entries

--- fluent/StorageRune.ts
+++ fluent/StorageRune.ts
@@ -84,10 +84,12 @@ export class StorageRune<
     return Rune
       .tuple([this.entriesRaw(props, blockHash).access(0), this.$key, this.$value])
       .map(([changeset, $key, $value]) =>
-        changeset!.changes.map(([k, v]) => [
-          $key.decode(hex.decode(k)),
-          v ? $value.decode(hex.decode(v)) : undefined,
-        ])
+        changeset
+          ? changeset.changes.map(([k, v]) => [
+            $key.decode(hex.decode(k)),
+            v ? $value.decode(hex.decode(v)) : undefined,
+          ])
+          : []
       )
       .unsafeAs<[Chain.Storage.Key<C, P, S>, Chain.Storage.Value<C, P, S>][]>()
   }
