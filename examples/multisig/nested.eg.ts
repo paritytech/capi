e04c17b2b82788476c1f1e8f555a00b3578cc1fb
Ryan Lee <ryan@parity.io>
2023-07-13 10:06:31 -0400

    fixes

--- examples/multisig/nested.eg.ts
+++ examples/multisig/nested.eg.ts
@@ -1,6 +1,7 @@
 /**
  * @title Nested Multisig
- * @description Create a multisig and use it as a signatory for another multisig.
+ * @description Create two multisigs with a parent-child relationship, where
+ * the first multisig (parent) is a signatory to the second multisig (child).
  */
 import { polkadotDev } from "@capi/polkadot-dev"
 import { assertEquals } from "asserts"
@@ -8,21 +9,24 @@ import { createDevUsers, is, Rune } from "capi"
 import { MultisigRune } from "capi/patterns/multisig"
 import { signature } from "capi/patterns/signature/polkadot"
 
+/// Create five dev users. The first three serve as signatories of
+/// the first multisig (`parent`) and the latter serve as signatories of the
+/// second multisig (`child`).
 const { alexa, billy, carol, david, ellie } = await createDevUsers()
 
-/// Create first multisig.
-const multisigA = MultisigRune.from(polkadotDev, {
+/// Create the parent multisig.
+const parent = MultisigRune.from(polkadotDev, {
   signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
   threshold: 2,
 })
 
-/// Create second multisig with first multisig as a signatory.
-const multisigB = MultisigRune.from(
+/// Create the child multisig with the parent multisig as the third signatory.
+const child = MultisigRune.from(
   polkadotDev,
   Rune.object({
     signatories: Rune.array([
       ...[david, ellie].map(({ publicKey }) => publicKey),
-      multisigA.accountId,
+      parent.accountId,
     ]),
     threshold: 2,
   }),
@@ -32,8 +36,8 @@ const multisigB = MultisigRune.from(
 await polkadotDev.Utility
   .batchAll({
     calls: Rune.array([
-      multisigA.fund(2_000_000_000_000n),
-      multisigB.fund(2_000_000_000_000n),
+      parent.fund(2_000_000_000_000n),
+      child.fund(2_000_000_000_000n),
     ]),
   })
   .signed(signature({ sender: alexa }))
@@ -42,20 +46,19 @@ await polkadotDev.Utility
   .finalized()
   .run()
 
-const multisigAFree = await polkadotDev.System.Account
-  .value(multisigA.accountId)
-  .unhandle(is(undefined))
-  .access("data", "free")
-  .run()
-console.log("Multisig A free:", multisigAFree)
-
-const multisigBFree = await polkadotDev.System.Account
-  .value(multisigB.accountId)
-  .unhandle(is(undefined))
-  .access("data", "free")
-  .run()
-console.log("Multisig B free:", multisigBFree)
-
 /// Verify existential deposit on both multisigs.
-assertEquals(multisigAFree, 2_000_000_000_000n)
-assertEquals(multisigBFree, 2_000_000_000_000n)
+const params = [
+  [parent.accountId, "Parent"],
+  [child.accountId, "Child"],
+] as const
+await Promise.all((params).map(async ([publicKey, name]) =>
+  assertEquals(
+    await polkadotDev.System.Account
+      .value(publicKey)
+      .unhandle(is(undefined))
+      .access("data", "free")
+      .dbg(`${name} free:`)
+      .run(),
+    2_000_000_000_000n,
+  )
+))
