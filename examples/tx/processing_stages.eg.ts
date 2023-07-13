b7649a69218cec4e7355d1705cf7c5b8daa16630
Harry Solovay <harrysolovay@gmail.com>
2023-07-13 15:45:50 -0400

    more processing stage methods

--- examples/tx/processing_stages.eg.ts
+++ examples/tx/processing_stages.eg.ts
@@ -5,7 +5,7 @@
  */
 
 import { westendDev } from "@capi/westend-dev"
-import { createDevUsers } from "capi"
+import { createDevUsers, Rune } from "capi"
 import { signature } from "capi/patterns/signature/polkadot"
 
 /// Create two dev users. Alexa will send the funds to Billy.
@@ -20,7 +20,28 @@ const sent = westendDev.Balances
   .signed(signature({ sender: alexa }))
   .sent()
 
-/// Attach callbacks to the pending status-specific promises.
-sent.ready().run().then(() => console.log("Ready"))
-sent.inBlock().run().then((hash) => console.log(`In block ${hash}`))
-sent.finalized().run().then((hash) => console.log(`Finalized ${hash}`))
+/// We can access and ingest lifecycle events as promises. For example,
+/// here we get a promise, which resolves to the list of optional broadcasts
+/// or false in the case that no broadcast status is emitted.
+sent.broadcast().run().then((values) =>
+  console.log("Broadcast:" + (values ? `"${values.join(`", "`)}"` : "true"))
+)
+
+/// For brevity, the following will use a `handle` helper, which wraps the
+/// `run` and formatting of the result. Each of these corresponds to a specific status.
+handle(sent.future(), "Future")
+handle(sent.ready(), "Ready")
+handle(sent.inBlock(), "In block")
+handle(sent.retracted(), "Redacted")
+handle(sent.finalityTimeout(), "Finality timeout")
+handle(sent.finalized(), "Finalized")
+
+/// hide-start
+async function handle<U>(rune: Rune<HandleT, U>, label: string) {
+  const value = await rune.run()
+  if (value) {
+    console.log(`${label}${typeof value === "undefined" ? "" : `: ${value}`}`)
+  }
+}
+type HandleT = string | boolean | undefined
+/// hide-end
