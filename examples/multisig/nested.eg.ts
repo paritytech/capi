6f48b40cd677cdc586da91fcd793fb872d7a61a4
Ryan Lee <ryan@parity.io>
2023-07-13 14:42:15 -0400

    fix

--- examples/multisig/nested.eg.ts
+++ examples/multisig/nested.eg.ts
@@ -1,32 +1,31 @@
 /**
  * @title Nested Multisig
- * @description Create two multisigs with a parent-child relationship, where
- * the first multisig (parent) is a signatory to the second multisig (child).
+ * @description Create two multisigs, one of which is a signatory for the other.
  */
 import { polkadotDev } from "@capi/polkadot-dev"
 import { assertEquals } from "asserts"
-import { createDevUsers, is, Rune } from "capi"
+import { createDevUsers, is, Rune, RunicArgs } from "capi"
 import { MultisigRune } from "capi/patterns/multisig"
 import { signature } from "capi/patterns/signature/polkadot"
 
 /// Create five dev users. The first three serve as signatories of
-/// the first multisig (`parent`) and the latter serve as signatories of the
-/// second multisig (`child`).
+/// the first multisig and the latter serve as signatories of the
+/// second multisig.
 const { alexa, billy, carol, david, ellie } = await createDevUsers()
 
-/// Create the parent multisig.
-const parent = MultisigRune.from(polkadotDev, {
+/// Create the first multisig.
+const nestedMultisig = MultisigRune.from(polkadotDev, {
   signatories: [alexa, billy, carol].map(({ publicKey }) => publicKey),
   threshold: 2,
 })
 
-/// Create the child multisig with the parent multisig as the third signatory.
-const child = MultisigRune.from(
+/// Create second multisig with first multisig as a signatory.
+const multisig = MultisigRune.from(
   polkadotDev,
   Rune.object({
     signatories: Rune.array([
+      nestedMultisig.accountId,
       ...[david, ellie].map(({ publicKey }) => publicKey),
-      parent.accountId,
     ]),
     threshold: 2,
   }),
@@ -36,8 +35,8 @@ const child = MultisigRune.from(
 await polkadotDev.Utility
   .batchAll({
     calls: Rune.array([
-      parent.fund(2_000_000_000_000n),
-      child.fund(2_000_000_000_000n),
+      nestedMultisig.fund(2_000_000_000_000n),
+      multisig.fund(2_000_000_000_000n),
     ]),
   })
   .signed(signature({ sender: alexa }))
@@ -46,19 +45,19 @@ await polkadotDev.Utility
   .finalized()
   .run()
 
+/// Read both multisig free balances from storage.
+const [nestedMultisigFree, multisigFree] = await Rune.tuple([
+  readFree(nestedMultisig.accountId).dbg("Nested Multisig free:"),
+  readFree(multisig.accountId).dbg("Multisig free:"),
+]).run()
+
 /// Verify existential deposit on both multisigs.
-const params = [
-  [parent.accountId, "Parent"],
-  [child.accountId, "Child"],
-] as const
-await Promise.all((params).map(async ([publicKey, name]) =>
-  assertEquals(
-    await polkadotDev.System.Account
-      .value(publicKey)
-      .unhandle(is(undefined))
-      .access("data", "free")
-      .dbg(`${name} free:`)
-      .run(),
-    2_000_000_000_000n,
-  )
-))
+assertEquals(nestedMultisigFree, 2_000_000_000_000n)
+assertEquals(multisigFree, 2_000_000_000_000n)
+
+function readFree<X>(...[publicKey]: RunicArgs<X, [Uint8Array]>) {
+  return polkadotDev.System.Account
+    .value(publicKey)
+    .unhandle(is(undefined))
+    .access("data", "free")
+}
