29a53cfdd1894fabb8e94d2efee4589aeb2725df
GitHub <noreply@github.com>
2023-07-16 00:30:36 +0000

    update dependencies

--- deps/cliffy.ts
+++ deps/cliffy.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts"
+export * from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts"
--- deps/std/async.ts
+++ deps/std/async.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.185.0/async/mod.ts"
+export * from "https://deno.land/std@0.194.0/async/mod.ts"
